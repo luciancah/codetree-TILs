6b562b8d108c9a1f1563007eb87b3cbf0e149c3c
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 01:42:11 +0900

    Title: 줄 세우기 2, Time: 240ms, Memory: 25MB, Status: Passed - CodeTree

--- 240403/줄 세우기 2/line-up-students-2.py
+++ 240403/줄 세우기 2/line-up-students-2.py
@@ -4,7 +4,8 @@ n = int(input())
 for i in range(n):
     ㅁㄱㄱ.append([int(x) for x in input().split()] + [i+1])
 
-ㅁㄱㄱ.sort(key=lambda x : [x[0], x[1]])
+ㅁㄱㄱ.sort(key=lambda x : [x[0], -x[1]])
+
 
 for i in ㅁㄱㄱ:
     print(*i)
