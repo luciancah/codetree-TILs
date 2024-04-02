8d9990f05186bfd66d7b6553e6537b9eaf63d2eb
Jinhyung Lee <luciancah@gmail.com>
2024-04-02 16:06:08 +0900

    Title: 특정 구간의 합, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/특정 구간의 합/sum-of-specific-intervals.py
+++ 240402/특정 구간의 합/sum-of-specific-intervals.py
@@ -6,3 +6,12 @@ for i in range(m):
     arr2.append(list(map(int, input().split())))
 
 print(n, m, arr, arr2)
+
+prefixSum = []
+
+for i in range(len(arr)):
+    if i == 0:
+        prefixSum.append(arr[i])
+    prefixSum.append(arr[i] + prefixSum[i])
+
+print(prefixSum)
