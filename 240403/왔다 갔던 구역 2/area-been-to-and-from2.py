f56770085df175cf5e8a5f91347cf77f6a54dcd9
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 17:38:18 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240403/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -2,7 +2,6 @@ n = int(input())
 positions = []
 current = 50
 line = [0] * 101
-
 for i in range(n):
     positions += [list(map(str, input().split()))]
 
@@ -10,11 +9,14 @@ for position in positions:
     dist, direction = position
 
     if direction == 'L':
-        for x in range(int(dist)):
-            line[current - x] += 1
+        for _ in range(int(dist)):
+            current -= 1
+            line[current] += 1
 
     if direction == 'R':
-        for x in range(int(dist)):
-            line[current + x - 1] += 1
+        for _ in range(int(dist)):
+            current += 1
+            line[current - 1] += 1
+
 
 print(len([x for x in line if x >= 2]))
