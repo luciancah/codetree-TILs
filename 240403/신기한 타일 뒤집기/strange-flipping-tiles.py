195a8a3ac8ce59b13edb7ef1d853e588944d3cda
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 17:56:49 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240403/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -3,27 +3,22 @@ positions = []
 current = 50
 
 
-line = [''] * 26
+line = [''] * 101
 for i in range(n):
     positions += [list(map(str, input().split()))]
 
 for position in positions:
     dist, direction = position
 
-
     if direction == 'L':
-        for _ in range(int(dist)):
-            current -= 1
-            line[current] = 'white'
-        print(line)
+        for i in range(int(dist)):
+            line[current - i] = 'white'
+        current -= int(dist) - 1
 
     if direction == 'R':
-        for _ in range(int(dist)):
-            current += 1
-            line[current - 1] = 'black'
-        print(line)
-
-print(line)
+        for i in range(int(dist)):
+            line[current + i] = 'black'
+        current += int(dist) - 1
 
 
 print(len([x for x in line if x == 'white']), len([x for x in line if x == 'black']))
