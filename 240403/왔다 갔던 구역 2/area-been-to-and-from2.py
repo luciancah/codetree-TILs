5e4d3df7756e4d5fc2dccb8b9bb4f4861a2ed36c
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 17:38:29 +0900

    Title: 왔다 갔던 구역 2, Time: 238ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240403/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,7 +1,7 @@
 n = int(input())
 positions = []
-current = 50
-line = [0] * 101
+current = 500
+line = [0] * 1001
 for i in range(n):
     positions += [list(map(str, input().split()))]
 
@@ -17,6 +17,7 @@ for position in positions:
         for _ in range(int(dist)):
             current += 1
             line[current - 1] += 1
+
 
 
 print(len([x for x in line if x >= 2]))
