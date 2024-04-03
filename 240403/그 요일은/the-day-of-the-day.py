6faa31484c68c1d161d4d6e8af81c1cc4a2522d0
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 12:49:34 +0900

    Title: 그 요일은, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/그 요일은/the-day-of-the-day.py
+++ 240403/그 요일은/the-day-of-the-day.py
@@ -9,17 +9,22 @@ if m2 - m1 >= 1:
     for i in range(m1+1, m2):
         daysDiff += dates[i]
 
-# m1월의 d1일부터 말일까지 남은 일 더하기
-daysDiff += (dates[m1] - d1)
+if m1 < m2:
+    # m1월의 d1일부터 말일까지 남은 일 더하기
+    daysDiff += (dates[m1] - d1)
 
-# m2월의 d2일만큼 더하기
-daysDiff += d2
+    # m2월의 d2일만큼 더하기
+    daysDiff += d2
 
-# A요일 diff만큼 빼기
+    # A요일 diff만큼 빼기
 
-daysDiff -= days.index(day)
+    daysDiff -= days.index(day)
+
+else:
+    daysDiff += (d2 - d1)
 
 print(daysDiff // 7 + 1 if daysDiff >= 0 else 0)
+
 
 '''
 m2월 d2일 - m1월 d1일
