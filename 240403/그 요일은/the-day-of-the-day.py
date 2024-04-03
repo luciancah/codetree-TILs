2498a96b1f7cfe4c7c1b63e97ce355c736024de4
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 12:55:31 +0900

    Title: 그 요일은, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/그 요일은/the-day-of-the-day.py
+++ 240403/그 요일은/the-day-of-the-day.py
@@ -5,7 +5,6 @@ dates = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 if m1 < m2:
-
     # 사이에 온전한 월의 일들 더하기
     if m2 - m1 >= 1:
         for i in range(m1+1, m2):
@@ -18,26 +17,10 @@ if m1 < m2:
     daysDiff += d2
 
     # A요일 diff만큼 빼기
-
     daysDiff -= days.index(day)
-
 
 else:
     daysDiff += (d2 - d1)
+    daysDiff -= days.index(day)
 
 print(daysDiff // 7 + 1 if daysDiff >= 0 else 0)
-
-
-'''
-m2월 d2일 - m1월 d1일
-
-6월 3일 - 8월 27일
-
-2 3 4 5 6 7월 += 31
-6월 30 - 3 27
-8월 27
-
-31 + 27 + 27 - 6 / 7
-
-월 A +6
-'''
