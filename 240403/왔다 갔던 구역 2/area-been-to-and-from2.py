31ba8a6d648a20073be5a7f52a3cd63de82351ba
Jinhyung Lee <luciancah@gmail.com>
2024-04-03 17:10:38 +0900

    Title: 왔다 갔던 구역 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,20 @@
+n = int(input())
+positions = []
+current = 50
+line = [0] * 101
+
+for i in range(n):
+    positions += [list(map(str, input().split()))]
+
+for position in positions:
+    dist, direction = position
+
+    if direction == 'L':
+        for x in range(int(dist)):
+            line[current - x] += 1
+
+    if direction == 'R':
+        for x in range(int(dist)):
+            line[current + x] += 1
+
+print(max(line))
