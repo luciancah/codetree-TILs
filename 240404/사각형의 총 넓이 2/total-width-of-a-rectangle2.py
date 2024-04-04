19c57b31f02694778e578ddacf6e0a9968d2d21b
Jinhyung Lee <luciancah@gmail.com>
2024-04-04 15:07:39 +0900

    Title: 사각형의 총 넓이 2, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240404/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240404/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -3,19 +3,19 @@ positions = []
 offset = 101
 count = 0
 
-matrix = [[0] * 202] * 202
+matrix = [[0 for _ in range(202)] for _ in range(202)]
 for i in range(n):
     positions += [list(map(int, input().split()))]
-
-for position in positions:
-    x1, y1, x2, y2 = position
+for x1, y1, x2, y2 in positions:
+    x1, y1 = x1 + offset, y1 + offset
+    x2, y2 = x2 + offset, y2 + offset
     for i in range(x1, x2):
         for j in range(y1, y2):
-            matrix[i+offset][j+offset] += 1
+            matrix[i][j] += 1
 
-for i in range(len(matrix)):
-    for j in range(len(matrix[0])):
-        if matrix[i][j] > 0:
-            count += 1
+for i in range(0, 202):
+        for j in range(0, 202):
+            if matrix[i][j] > 0:
+                count += 1
 
-print(matrix)
+print(count)
