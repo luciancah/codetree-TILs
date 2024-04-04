a27c3a1609ad0b5f08a8a248175e40854f0db701
Jinhyung Lee <luciancah@gmail.com>
2024-04-04 15:31:28 +0900

    Title: 색종이의 총 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240404/색종이의 총 넓이/the-total-area-of-colored-paper..py
@@ -0,0 +1,23 @@
+n = int(input())
+positions = []
+offset = 101
+count = 0
+
+matrix = [[0 for _ in range(202)] for _ in range(202)]
+for i in range(n):
+    positions += [list(map(int, input().split()))]
+for index, values in enumerate(positions):
+    x1, y1 = values
+    x1, y1 = x1 + offset, y1 + offset
+    x2, y2 = x1 + 8, y2 + 8
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            matrix[i][j] += c
+
+for i in range(0, 2002):
+        for j in range(0, 2002):
+            
+            if matrix[i][j]:
+                count += 1
+
+print(count)
