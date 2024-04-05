e1042f305a0d17c86f8a63b799ea3acbcfb7b326
Jinhyung Lee <luciancah@gmail.com>
2024-04-05 13:26:13 +0900

    Title: 만나는 그 순간, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/만나는 그 순간/the-moment-we-meet.py
+++ 240405/만나는 그 순간/the-moment-we-meet.py
@@ -1,45 +1,36 @@
 n, m = list(map(int, input().split()))
+position = 0
 moves = []
 a_pos = []
 b_pos = []
 for i in range(n+m):
     moves += [list(map(str,input().split()))]
-
+ 
 a_moves = moves[0:n]
 b_moves = moves[n:]
 
-position = 0
-for dir, dist in a_moves:
-    if dir == 'R':
-        position += int(dist)
-    else:
-        position -= int(dist)
-    a_pos.append(position)
+print(a_moves)
 
-position = 0
-for dir, dist in b_moves:
-    if dir == 'R':
-        position += int(dist)
-    else:
-        position -= int(dist)
-    b_pos.append(position)
+for move in a_moves:
+    dir, dist = move
+    dist = int(dist)
 
-a_meet = -1
-count = 0
-for i, a in enumerate(a_pos):
-    print('a', i, a, a_moves[i][1], b_pos)
-    count += int(a_moves[i][1])
-    if a in b_pos:
-        a_meet = count
-        break
+    for _ in range(dist):
+        if dir == 'L':
+            position -= 1
+        if dir == 'R':
+            position += 1
+        a_pos.append(position)
 
-b_meet = -1
-count = 0
-for i, b in enumerate(b_pos):
-    print('b', i, b, b_moves[i][1], a_pos)
-    count += int(b_moves[i][1])
-    if a in a_pos:
-        b_meet = count
-        break
+for move in b_moves:
+    dir, dist = move
+    dist = int(dist)
 
-print(max(a_meet, b_meet))
+    for _ in range(dist):
+        if dir == 'L':
+            position -= 1
+        if dir == 'R':
+            position += 1
+        b_pos.append(position)
+
+print(a_pos, b_pos)
