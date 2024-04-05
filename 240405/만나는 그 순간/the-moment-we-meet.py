0a246fb17e348210e401f893b2c603b2818de5ab
Jinhyung Lee <luciancah@gmail.com>
2024-04-05 12:40:29 +0900

    Title: 만나는 그 순간, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/만나는 그 순간/the-moment-we-meet.py
+++ 240405/만나는 그 순간/the-moment-we-meet.py
@@ -1,8 +1,39 @@
 n, m = list(map(int, input().split()))
 moves = []
-a_moves = []
-b_moves = []
+a_pos = []
+b_pos = []
 for i in range(n+m):
-    moves += [map(int,input().split())]
+    moves += [list(map(str,input().split()))]
 
-moves
+a_moves = moves[0:n]
+b_moves = moves[n:]
+
+position = 0
+for dir, dist in a_moves:
+    if dir == 'R':
+        position += int(dist)
+    else:
+        position -= int(dist)
+    a_pos.append(position)
+
+position = 0
+for dir, dist in b_moves:
+    if dir == 'R':
+        position += int(dist)
+    else:
+        position -= int(dist)
+    b_pos.append(position)
+
+a_meet = -1
+for i, a in enumerate(a_pos):
+    if a in set(b_pos):
+        a_meet = a
+    break
+
+b_meet = -1
+for i, b in enumerate(b_pos):
+    if a in set(a_pos):
+        b_meet = b
+    break
+
+print(max(a_meet, b_meet))
