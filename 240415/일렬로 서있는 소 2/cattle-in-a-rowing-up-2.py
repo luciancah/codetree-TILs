8a0ba28c3341b245c83b3439d32807fc0dd276c6
Jinhyung Lee <luciancah@gmail.com>
2024-04-15 18:07:33 +0900

    Title: 일렬로 서있는 소 2, Time: 139ms, Memory: 25MB, Status: Passed - CodeTree

--- 240415/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240415/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -3,8 +3,8 @@ cows = list(map(int, input().split()))
 count = 0
 
 for i in range(len(cows)):
-    for j in range(i, len(cows)):
-        for k in range(j, len(cows)):
+    for j in range(i + 1, len(cows)):
+        for k in range(j + 1, len(cows)):
             if cows[i] <= cows[j] and cows[j] <= cows[k]:
                 count += 1
 
