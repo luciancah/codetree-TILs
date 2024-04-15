3393e755bf786862064fc3bde7aed3bbc93cd2e7
Jinhyung Lee <luciancah@gmail.com>
2024-04-15 17:43:21 +0900

    Title: 모이자, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240415/모이자/gather.py
+++ 240415/모이자/gather.py
@@ -4,12 +4,13 @@ n = int(input())
 arr = list(map(int, input().split()))
 min_distance = sys.maxsize
 
-for i in range(len(arr)):
+for i in range(1, len(arr)+1):
     diff = 0
-    for j in range(len(arr)):
+    for j in range(1, len(arr)+1):
         # diff += abs(arr[i] - arr[j])
-        diff += abs(i - j) * arr[j]
+        diff += abs(i - j) * arr[j-1]
     
+
     min_distance = min(min_distance, diff)
 
 print(diff)
