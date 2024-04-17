0e384a442439c3f2c036e099d25661b42d249c3b
Jinhyung Lee <luciancah@gmail.com>
2024-04-17 18:07:23 +0900

    Title: 체크판위에서 2, Time: 162ms, Memory: 45MB, Status: Passed - CodeTree

--- 240417/체크판위에서 2/on-the-checkboard-2.py
+++ 240417/체크판위에서 2/on-the-checkboard-2.py
@@ -4,8 +4,8 @@ for i in range(r):
     maps.append(list(map(str,input().split())))
 
 count = 0
-for i in range(r):
-    for j in range(c):
+for i in range(1, r):
+    for j in range(1, c):
         for k in range(i + 1, r - 1):
             for l in range(j + 1, c - 1):
                 if maps[0][0] != maps[i][j] and \
