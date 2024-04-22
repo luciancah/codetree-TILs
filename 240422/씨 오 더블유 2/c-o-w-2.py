dc46271f03a65f9f6240f29b94e1a66c8d2490a0
Jinhyung Lee <luciancah@gmail.com>
2024-04-22 17:21:34 +0900

    Title: 씨 오 더블유 2, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

--- 240422/씨 오 더블유 2/c-o-w-2.py
+++ 240422/씨 오 더블유 2/c-o-w-2.py
@@ -3,11 +3,11 @@ input_string = input()
 count = 0
 
 for i in range(n):
-    if input_string[i] = 'C':
+    if input_string[i] == 'C':
         for j in range(i, n):
-            if input_string[j] = 'O':
+            if input_string[j] == 'O':
                 for k in range(j, n):
-                    if input_string[k] = 'W':
+                    if input_string[k] == 'W':
                         count += 1
 
 print(count)
