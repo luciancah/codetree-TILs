9600fc6e38beb873b83a82f948f32303ba789480
Jinhyung Lee <luciancah@gmail.com>
2024-04-22 17:14:11 +0900

    Title: 괄호 쌍 만들어주기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240422/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240422/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,10 +1,10 @@
 input_string = input()
 count = 0
 
-for i in range(len(input_string)):
-    if input_string[i-1] == '(' and input_string[i] == '(':
-        for j in range(i+1, len(input_string)):
-            if input_string[j-1] == ')' and input_string[j] == ')':
+for i in range(len(input_string)-1):
+    if input_string[i] == '(' and input_string[i+1] == '(':
+        for j in range(i+2, len(input_string))-1:
+            if input_string[j] == ')' and input_string[j+1] == ')':
                 count += 1
 
 print(count)
