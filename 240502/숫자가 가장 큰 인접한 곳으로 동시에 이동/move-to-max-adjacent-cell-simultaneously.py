d5615335b8a9e0dd79a20f8ecc3658db3aa8c77a
Jinhyung Lee <luciancah@gmail.com>
2024-05-02 21:56:25 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,48 @@
+def print_arr(arr):
+    print('-----------------')
+    for i in arr:
+        print(i)
+
+n, m, t = map(int, input().split())
+matrix = []
+count = [[0 for col in range(n)] for row in range(n)]
+n_count = [[0 for col in range(n)] for row in range(n)]
+for _ in range(n):
+    matrix.append([int(x) for x in input().split()])
+for _ in range(m):
+    r, c = map(int, input().split())
+    count[r - 1][c - 1] = 1
+
+# print_arr(matrix)
+dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+for _ in range(t):
+    for i in range(n):
+        for k in range(n):
+            if count[i][k]:
+                x, y = k, i
+                max_val = matrix[y][x]
+                mx, my = x, y
+                for j in range(4):
+                    nx, ny = x + dxs[j], y + dys[j]
+                    if nx > n - 1 or nx < 0 or ny > n - 1 or ny < 0:
+                        continue
+                    if matrix[ny][nx] > max_val:
+                        max_val = matrix[ny][nx]
+                        mx = nx
+                        my = ny
+                n_count[mx][my] += 1
+    # print_arr(count)
+    # print_arr(n_count)
+    for item in n_count:
+        for idx in range(len(item)):
+            if item[idx] >= 2:
+                item[idx] = 0
+    # print_arr(n_count)
+    count = n_count
+
+total = 0
+for item in count:
+    for idx in range(len(item)):
+        if item[idx]:
+            total += 1
+print(total)
