1cbfcbe2208e8fa336c79edeb763f7fc57004dad
Jinhyung Lee <luciancah@gmail.com>
2024-05-03 16:57:23 +0900

    Title: 숨은 단어 찾기 2, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 240503/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,57 +1,37 @@
 n, m = list(map(int, input().split()))
 arr = []
+
 for _ in range(n):
     arr.append(input())
 
-count = 0
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
 
-for i in range(n - 2):
-    for j in range(m - 2):
-        if arr[i][j] == 'L':
-            if arr[i][j+1] == arr[i][j+2] == 'E':
-                # print(1, count, i, j)
-                count += 1
-            if arr[i+1][j] == arr[i+2][j] == 'E':
-                # print(1, count, i, j)
-                count += 1
-            if arr[i+1][j+1] == arr[i+2][j+2] == 'E':
-                # print(1, count, i, j)
-                count += 1
+dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
 
-        if arr[i][j] == 'E':
-            if arr[i][j+1] == 'E' and arr[i][j+2] == 'L':
-                # print(2, count, i, j)
-                count += 1
-            if arr[i+1][j] == 'E' and arr[i+2][j] == 'L':
-                # print(2, count, i, j)
-                count += 1
-            if arr[i+1][j+1] == 'E' and arr[i+2][j+2] == 'L':
-                # print(2, count, i, j)
-                count += 1
+cnt = 0
+for i in range(n):
+	for j in range(m):
+		
+		if arr[i][j] != 'L':
+			continue
+		
+		for dx, dy in zip(dxs, dys):
+			curt = 1
+			curx = i
+			cury = j
+			while True:
+				nx = curx + dx
+				ny = cury + dy
+				if in_range(nx, ny) == False:
+					break
+				if arr[nx][ny] != 'E':
+					break
+				curt += 1
+				curx = nx
+				cury = ny
+			
+			if curt >= 3:
+				cnt += 1
 
-for i in range(n-3, n):
-    for j in range(m - 2):
-        if arr[i][j] == 'L':
-            if arr[i][j+1] == arr[i][j+2] == 'E':
-                # print(3, count, i, j)
-                count += 1
-
-        if arr[i][j] == 'E':
-            if arr[i][j+1] == 'E' and arr[i][j+2] == 'L':
-                # print(3, count, i, j)
-                count += 1
-
-for i in range(n-2):
-    for j in range(m - 3, m):
-        if arr[i][j] == 'L':
-            if arr[i+1][j] == arr[i+2][j] == 'E':
-                # print(4, count, i, j)
-                count += 1
-
-        if arr[i][j] == 'E':
-            if arr[i+1][j] == 'E' and arr[i+2][j] == 'L':
-                # print(4, count, i, j)
-                count += 1
-
-
-print(count)
+print(cnt)
