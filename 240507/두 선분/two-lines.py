50a18c9902d2e0d9db1325a364a45d8b549f6ce7
Jinhyung Lee <luciancah@gmail.com>
2024-05-07 17:30:59 +0900

    Title: 두 선분, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/두 선분/two-lines.py
+++ 240507/두 선분/two-lines.py
@@ -1,6 +1,13 @@
 a, b, c, d = list(map(int, input().split()))
 
-if (b < c):
-    print('notintersecting')
-else:
-    print('intersecting')
+if (a < c):
+    if (b < c):
+        print('notintersecting')
+    else:
+        print('intersecting')
+
+if (c < a):
+    if (c < b):
+        print('notintersecting')
+    else:
+        print('intersecting')
