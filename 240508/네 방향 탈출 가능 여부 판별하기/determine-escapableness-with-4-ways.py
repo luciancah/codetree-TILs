3919e8942b4f8621b6bd102c56366d29b6e848b5
Jinhyung Lee <luciancah@gmail.com>
2024-05-08 21:25:06 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 181ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240508/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+n, m = map(int, input().split())
+snake_safe = [[int(x) for x in input().split()] for i in range(n)]
+visited = [[0 for col in range(n)] for row in range(m)]
+
+q = deque()
+q.append((0, 0))
+dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0] #상하좌우
+while q:
+    x, y = q.popleft()
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0 and snake_safe[nx][ny]:
+            visited[nx][ny] = 1
+            q.append((nx, ny))
+
+if visited[-1][-1] == 1:
+    print(1)
+print(0)
