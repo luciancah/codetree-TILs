4998d61f270f790f997221aef9e76ac05a7d652c
Jinhyung Lee <luciancah@gmail.com>
2024-05-08 20:05:19 +0900

    Title: 그래프 탐색, Time: 177ms, Memory: 26MB, Status: Passed - CodeTree

--- 240508/그래프 탐색/graph-traversal.py
+++ 240508/그래프 탐색/graph-traversal.py
@@ -8,8 +8,7 @@ visited = [False for _ in range(n+1)]
 for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
-
-
+    graph[y].append(x)
 
 def DFS(v):
     for curr_v in graph[v]:
@@ -17,10 +16,9 @@ def DFS(v):
             visited[curr_v] = True
             # count = count + 1
             DFS(curr_v)
-
-
+visited[1] = True
 DFS(1)
 for i in visited:
     if i == True:
         count += 1
-print(count)
+print(count-1)
