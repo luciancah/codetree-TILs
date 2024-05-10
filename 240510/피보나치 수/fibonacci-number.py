ff86a71b05d00754dfe69e3f9306cae642a62cc6
Jinhyung Lee <luciancah@gmail.com>
2024-05-10 19:37:46 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240510/피보나치 수/fibonacci-number.py
+++ 240510/피보나치 수/fibonacci-number.py
@@ -12,4 +12,13 @@ def pick_fib(n):
         memo[n] = pick_fib(n - 1) + pick_fib(n - 2)
     return memo[n]
 
-print(pick_fib(n))
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 1
+
+def pick_fib_tab(n):
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    print(dp[n])
+
+pick_fib_tab(n)
