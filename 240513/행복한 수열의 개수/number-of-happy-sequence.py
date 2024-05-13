1fa4dc1a819442bf12c8d2a03d547645ce644776
Jinhyung Lee <luciancah@gmail.com>
2024-05-13 14:52:44 +0900

    Title: 행복한 수열의 개수, Time: 150ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240513/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240513/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,26 +8,30 @@ arr2 = [list(x) for x in zip(*arr)]
 fullcount = 0
 count = 0
 
-# 가로순회
-for a1 in arr:
-    for i in range(1, len(a1)):
-        if a1[i-1] == a1[i]:
-            count += 1
-            if count >= m-1:
-                fullcount += 1
-                break
-        else:
-            count = 0
+if m == 2:
+    print(2*n)
 
-# 가로순회
-for a2 in arr2:
-    for i in range(1, len(a2)):
-        if a2[i-1] == a2[i]:
-            count += 1
-            if count >= m-1:
-                fullcount += 1
-                break
-        else:
-            count = 0
+else:
+    # 가로순회
+    for a1 in arr:
+        for i in range(1, len(a1)):
+            if a1[i-1] == a1[i]:
+                count += 1
+                if count >= m-1:
+                    fullcount += 1
+                    break
+            else:
+                count = 0
 
-print(fullcount)
+    # 가로순회
+    for a2 in arr2:
+        for i in range(1, len(a2)):
+            if a2[i-1] == a2[i]:
+                count += 1
+                if count >= m-1:
+                    fullcount += 1
+                    break
+            else:
+                count = 0
+
+    print(fullcount)
