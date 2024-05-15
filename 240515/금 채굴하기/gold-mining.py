e1ce754c7f83f2ec6ee9603d4b01e22ee7a97c37
Jinhyung Lee <luciancah@gmail.com>
2024-05-15 16:48:39 +0900

    Title: 금 채굴하기, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240515/금 채굴하기/gold-mining.py
+++ 240515/금 채굴하기/gold-mining.py
@@ -1,35 +1,33 @@
 n, m = list(map(int, input().split()))
+k = 1
 arr = []
 max_price = 0
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-cb = m*m+(m+1)*(m+1)
+for k in range(n+2):
+    cb = k*k+(k+1)*(k+1)
 
-offset = m
+    for _ in range(k):
+        arr.insert(0, [0] * n)
+        arr.append([0] * n)
 
-for _ in range(offset):
-    arr.insert(0, [0] * n)
-    arr.append([0] * n)
+    for a in arr:
+        for _ in range(k):
+            a.insert(0, 0)
+            a.append(0)
 
-for a in arr:
-    for _ in range(offset):
-        a.insert(0, 0)
-        a.append(0)
+    for i in range(k, n+k):
+        for j in range(k, n+k):
+            price = 0
+            # 마름모 탐색
+            for x in range(i-k, i+k):
+                for y in range(j-k, j+k):
+                    if abs(x-i) + abs(y-j) > k:
+                        continue
+                    price += arr[x][y]
+            if m * price >= cb:
+                max_price = max(price, max_price)
 
-print(arr)  
-
-# for i in range(offset, n+offset):
-#     for j in range(offset, n+offset):
-#         price = 0
-#         # 마름모 탐색
-#         for x in range(i-m, i+m):
-#             print(arr[x])
-#             for y in range(j-m, j+m):
-#                 if abs(x-i) + abs(y-j) > m:
-#                     continue
-#                 price += arr[x][y]
-#         max_price = max(price, max_price)
-
-# print(max_price)
+print(max_price)
