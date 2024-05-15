d7eb0ff868ea84e489f85ddb52ed68c846eacf61
Jinhyung Lee <luciancah@gmail.com>
2024-05-15 17:43:08 +0900

    Title: 금 채굴하기, Time: 672ms, Memory: 28MB, Status: Passed - CodeTree

--- 240515/금 채굴하기/gold-mining.py
+++ 240515/금 채굴하기/gold-mining.py
@@ -1,35 +1,25 @@
 import copy
 
 n, m = list(map(int, input().split()))
-tmp = []
+arr = []
 max_count = 0
 
 for _ in range(n):
-    tmp.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
 
 k = 0
 while k <= 2 * (n-1):
-    arr = copy.deepcopy(tmp)
     cb = k*k+(k+1)*(k+1)
-
-    for _ in range(k):
-        arr.insert(0, [0] * n)
-        arr.append([0] * n)
-
-    for a in range(len(arr)):
-        for _ in range(k):
-            arr[a].insert(0, 0)
-            arr[a].append(0)
-
-    for i in range(k, n+k):
-        for j in range(k, n+k):
+    for i in range(n):
+        for j in range(n):
             count = 0
             # 마름모 탐색
             for x in range(i-k, i+k+1):
                 for y in range(j-k, j+k+1):
                     if abs(x-i) + abs(y-j) > k:
                         continue
-                    count += arr[x][y]
+                    if 0<=x < n and 0<=y<n:
+                        count += arr[x][y]
             if m * count >= cb:
                 max_count = max(count, max_count)
     k+=1
