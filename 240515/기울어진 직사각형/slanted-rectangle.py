4fbed820c019d92151143f75decf68cf547deb79
Jinhyung Lee <luciancah@gmail.com>
2024-05-15 18:21:58 +0900

    Title: 기울어진 직사각형, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240515/기울어진 직사각형/slanted-rectangle.py
+++ 240515/기울어진 직사각형/slanted-rectangle.py
@@ -3,64 +3,52 @@ arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-def max_tilted_rectangle_sum(grid):
-    n = len(grid)
-    
-    def in_bounds(x, y):
+def max_diagonal_rectangle_sum(n, grid):
+    def is_valid(x, y):
         return 0 <= x < n and 0 <= y < n
-    
+
     max_sum = 0
-    
-    # Iterate over every possible starting point
-    for x in range(n):
-        for y in range(n):
-            # Try every possible length for each direction d1, d2, d3, d4
-            for d1 in range(1, n):  # d1 = top-right
-                for d2 in range(1, n):  # d2 = bottom-right
-                    if not in_bounds(x + d1, y + d1) or not in_bounds(x + d1 + d2, y + d1 - d2):
-                        continue  # If moving d1 top-right and then d2 bottom-right goes out of bounds, skip
+
+    # 모든 시작점 (i, j) 탐색
+    for i in range(n):
+        for j in range(n):
+            # 길이를 늘려가며 기울어진 직사각형 탐색
+            for l1 in range(1, n):
+                x1, y1 = i + l1, j + l1
+                if not is_valid(x1, y1):
+                    break
+                
+                for l2 in range(1, n):
+                    x2, y2 = x1 + l2, y1 - l2
+                    if not is_valid(x2, y2):
+                        break
                     
-                    for d3 in range(1, n):  # d3 = bottom-left
-                        if not in_bounds(x + d1 + d2 - d3, y + d1 - d2 - d3):
-                            continue  # If further d3 bottom-left goes out of bounds, skip
+                    for l3 in range(1, n):
+                        x3, y3 = x2 - l3, y2 - l3
+                        if not is_valid(x3, y3):
+                            break
                         
-                        for d4 in range(1, n):  # d4 = top-left
-                            # Calculate final position after moving d4 top-left
-                            final_x = x + d1 + d2 - d3 - d4
-                            final_y = y + d1 - d2 - d3 + d4
-                            
-                            if final_x != x or final_y != y:
-                                continue  # If it doesn't close the rectangle, skip
-                            
-                            # Now calculate the sum of this tilted rectangle
-                            current_sum = 0
-                            # Segment 1: top-right
-                            for i in range(d1):
-                                xi, yi = x + i, y + i
-                                if not in_bounds(xi, yi):
-                                    break
-                                current_sum += grid[xi][yi]
-                            # Segment 2: bottom-right
-                            for i in range(d2):
-                                xi, yi = x + d1 + i, y + d1 - i
-                                if not in_bounds(xi, yi):
-                                    break
-                                current_sum += grid[xi][yi]
-                            # Segment 3: bottom-left
-                            for i in range(d3):
-                                xi, yi = x + d1 + d2 - i, y + d1 - d2 - i
-                                if not in_bounds(xi, yi):
-                                    break
-                                current_sum += grid[xi][yi]
-                            # Segment 4: top-left
-                            for i in range(d4):
-                                xi, yi = x + d1 + d2 - d3 - i, y + d1 - d2 - d3 + i
-                                if not in_bounds(xi, yi):
-                                    break
-                                current_sum += grid[xi][yi]
-                            
-                            max_sum = max(max_sum, current_sum)
-    
+                        x4, y4 = x3 - l1, y3 + l1
+                        if not is_valid(x4, y4):
+                            continue
+                        
+                        if (x4, y4) == (i, j):
+                            # 기울어진 직사각형의 합 계산
+                            curr_sum = 0
+                            # ↘
+                            for k in range(l1):
+                                curr_sum += grid[i + k][j + k]
+                            # ↙
+                            for k in range(l2):
+                                curr_sum += grid[x1 + k][y1 - k]
+                            # ↖
+                            for k in range(l3):
+                                curr_sum += grid[x2 - k][y2 - k]
+                            # ↗
+                            for k in range(l1):
+                                curr_sum += grid[x3 - k][y3 + k]
+                            max_sum = max(max_sum, curr_sum)
+
     return max_sum
 
-print(max_tilted_rectangle_sum(arr))
+print(max_diagonal_rectangle_sum(n, arr))
