19a66edbef01b4fbb64a109e9cb0acfa3da7cba5
Jinhyung Lee <luciancah@gmail.com>
2024-05-15 16:38:38 +0900

    Title: 금 채굴하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240515/금 채굴하기/gold-mining.py
+++ 240515/금 채굴하기/gold-mining.py
@@ -1,15 +1,35 @@
 n, m = list(map(int, input().split()))
 arr = []
-count = 0
+max_price = 0
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
 cb = m*m+(m+1)*(m+1)
 
-offset = m // 2
+offset = m
 
 for _ in range(offset):
-    arr.insert(0, [0 * m])
+    arr.insert(0, [0] * n)
+    arr.append([0] * n)
 
-print(arr)
+for a in arr:
+    for _ in range(offset):
+        a.insert(0, 0)
+        a.append(0)
+
+print(arr)  
+
+# for i in range(offset, n+offset):
+#     for j in range(offset, n+offset):
+#         price = 0
+#         # 마름모 탐색
+#         for x in range(i-m, i+m):
+#             print(arr[x])
+#             for y in range(j-m, j+m):
+#                 if abs(x-i) + abs(y-j) > m:
+#                     continue
+#                 price += arr[x][y]
+#         max_price = max(price, max_price)
+
+# print(max_price)
