298caeb19a2fb1df30aed54d5e4cf168958192d6
Jinhyung Lee <luciancah@gmail.com>
2024-05-16 14:53:03 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240516/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -5,10 +5,10 @@ for _ in range(n):
     arr.append(list(map(int, input().split())))
 
 def in_range_1(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x and x < n and 0 <= y and y < m
 
-def in_range_2(bx, by, x, y):
-    return 0 <= x and x < n and 0 <= y and y < n and (bx <= x or by <= y)
+def in_range_2(basex, basey, x, y):
+    return (basex <= x or basey <= y) and x < n and y < m
 
 
 max_sum = -9999999
@@ -23,20 +23,22 @@ for ai in range(n):
                 if not in_range_1(ai2, aj2):
                     continue
                 sum1 += arr[ai2][aj2]
-                print(sum1)
+                print('sum1', sum1)
 
                 for bi in range(n):
                     for bj in range(m):
                         sum2 = 0
-                        for bi2 in range(1, n):
-                            for bj2 in range(1, m):
+                        for bi2 in range(n):
+                            for bj2 in range(m):
                                 if not in_range_2(ai2, aj2, bi2, bj2):
                                     continue
                                 sum2 += arr[bi2][bj2]
-
+                        print('sum2', sum2)
+                print(sum1)
                 temp_max = max_sum
                 max_sum = max(max_sum, sum1+sum2)
                 if temp_max != max_sum:
-                    print(sum1, sum2)
+                    print('사각형 1번', sum1, ai, aj, ai2, aj2)
+                    print('사각형 2번', sum2, bi, bj, bi2, bj2)
 
 print(max_sum)
