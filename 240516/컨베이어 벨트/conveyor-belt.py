e54ba6036953c1acdbdc7ec9e0184ae344754c7d
Jinhyung Lee <luciancah@gmail.com>
2024-05-16 18:10:01 +0900

    Title: 컨베이어 벨트, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240516/컨베이어 벨트/conveyor-belt.py
+++ 240516/컨베이어 벨트/conveyor-belt.py
@@ -3,8 +3,9 @@ arr = []
 for i in range(2):
     arr += list(map(int,input().split()))
 
-last = arr.pop()
-arr.insert(0, last)
+for i in range(t):
+    last = arr.pop()
+    arr.insert(0, last)
 
 print(*arr[:len(arr)//2])
 print(*arr[len(arr)//2:])
