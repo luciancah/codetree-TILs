6ed60c3a911a1b71e4d445b125c78afc3cb90823
Jinhyung Lee <luciancah@gmail.com>
2024-05-16 16:38:39 +0900

    Title: 양수 직사각형의 최대 크기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240516/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -7,9 +7,8 @@ for _ in range(n):
 def in_range(x0, y0, x1, y1):
     if not (0 <= x0 <= x1 <= m and 0 <= y0 <= y1 <= n):
         return False
-    for y in range(y0, y1):
-        for x in range(x0, x1):
-            print(arr[y][x])
+    for y in range(y0, y1+1):
+        for x in range(x0, x1+1):
             if arr[y][x] < 0:
                 return False
     
@@ -17,19 +16,14 @@ def in_range(x0, y0, x1, y1):
 
 max_size = 0
 
-print('in range', in_range(0,1,3,3))
+for y0 in range(n):
+    for x0 in range(m):
+        size = 0
+        for y1 in range(y0, n):
+            for x1 in range(x0, n):
+                if not in_range(x0, y0, x1, y1):
+                    continue
+                size = (y1 - y0 + 1) * (x1 - x0 + 1)
+                max_size = max(size, max_size)
 
-# for y0 in range(n):
-#     for x0 in range(m):
-#         size = 0
-#         for y1 in range(y0, n):
-#             for x1 in range(x0, n):
-#                 if not in_range(x0, y0, x1, y1):
-#                     continue
-#                 size = (y1 - y0 + 1) * (x1 - x0 + 1)
-#                 temp_size = max_size
-#                 max_size = max(size, max_size)
-#                 if max_size != temp_size:
-#                     print(max_size, y0, x0, y1, x1)
-
-# print(max_size if max_size != 0 else -1)
+print(max_size if max_size != 0 else -1)
