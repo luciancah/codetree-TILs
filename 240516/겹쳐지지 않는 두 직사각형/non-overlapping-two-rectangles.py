31645073ca7bca7b60d7f94c884e7ec76a8478df
Jinhyung Lee <luciancah@gmail.com>
2024-05-16 15:26:09 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240516/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240516/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -8,7 +8,8 @@ def in_range_1(x, y,x1,y1):
     return 0 <= x <= x1 < n and 0 <= y <= y1 < m
 
 def in_range_2(basex, basey, x, y, x1, y1):
-    return (basex-1 <= x or basey-1 <= y) and 0 <= x <= x1 < n and 0 <= y <= y1 < m
+    return ((basex <= x and basey < y) or (basex < x and basey <= y)) and 0 <= x <= x1 < n and 0 <= y <= y1 < m
+
 
 def calc_sum(bx, by, x, y):
     s = 0
@@ -29,20 +30,21 @@ for ai in range(n):
                 if not in_range_1(ai, aj, ai2, aj2):
                     continue
                 sum1 = calc_sum(ai, aj, ai2, aj2)
-                print('sum1', sum1)
-
+                # print('sum1', sum1)
                 for bi in range(n):
                     for bj in range(m):
                         for bi2 in range(n):
                             for bj2 in range(m):
+                                # if not in_range_1(bi,bj,bi2,bj2):
+                                #     continue
                                 if not in_range_2(ai2, aj2, bi, bj, bi2, bj2):
                                     continue
                                 sum2 = calc_sum(bi, bj, bi2, bj2)
                                 # print('sum2', sum2)
                                 temp_max = max_sum
                                 max_sum = max(max_sum, sum1+sum2)
-                                if temp_max != max_sum:
-                                    print('사각형 1번', sum1, ai, aj, ai2, aj2)
-                                    print('사각형 2번', sum2, bi, bj, bi2, bj2)
+                                # if temp_max != max_sum:
+                                #     print('사각형 1번', sum1, ai, aj, ai2, aj2)
+                                #     print('사각형 2번', sum2, bi, bj, bi2, bj2)
 
 print(max_sum)
