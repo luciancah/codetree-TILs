3578d57282d86b1a157d512da27bbd9a4c4ffe6d
Jinhyung Lee <luciancah@gmail.com>
2024-05-21 20:38:00 +0900

    Title: 기울어진 직사각형의 회전, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240521/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240521/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -59,16 +59,16 @@ def rotate_cw(c0x, c0y, c1x, c1y, c2x, c2y, c3x, c3y):
     count = 0
     for ur in range(c2x, c3x):
         # 1, 3 -> 3, 1
-        grid[ur][c3y-count] = grid[ur+1][c3y-count-1]
+        grid[ur][c2y-count] = grid[ur+1][c2y-count-1]
         count += 1
 
     count = 0
     for ul in range(c3x, c0x):
         # 3, 1 -> 4, 2
-        grid[ul][c0y+count] = grid[ul+1][c0y+count+1]
+        grid[ul][c3y+count] = grid[ul+1][c3y+count+1]
         count += 1
 
-    grid[c0x+1][c0y-1] = temp
+    grid[c0x-1][c0y-1] = temp
 
 
 if sq[-1] == 0:
