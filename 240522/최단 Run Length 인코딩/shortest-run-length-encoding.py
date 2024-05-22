c15f649c0da0038eda20231e15c58c96f696b55f
Jinhyung Lee <luciancah@gmail.com>
2024-05-22 12:22:52 +0900

    Title: 최단 Run Length 인코딩, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240522/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240522/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -6,4 +6,35 @@
 - 완탐 돌려서 길이 미니멈 만들기
 '''
 
-str = input()
+s = input()
+
+def shift(s):
+    last = s[-1]
+    new = last + s[:-1]
+
+    return new
+
+def rle(s):
+    s = s + '0'
+    count = 1
+    char = '_'
+    ans = ''
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            char = s[i]
+            count += 1
+        else:
+            ans += char
+            ans += str(count)
+            count = 1
+            char = s[i]
+    return len(ans)
+
+
+min_rle_len = 99999
+for i in range(len(s)):
+    s = shift(s)
+    rle_len = rle(s)
+    min_rle_len = min(min_rle_len, rle_len)
+
+print(min_rle_len)
