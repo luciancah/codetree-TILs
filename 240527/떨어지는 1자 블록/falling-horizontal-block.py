fb71abb50cf3be016843dabdba21a2e963a82109
Jinhyung Lee <luciancah@gmail.com>
2024-05-27 14:18:32 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240527/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,22 @@
+n, m, k = list(map(int, input().split()))
+grid = []
+
+for _ in range(n):
+    grid.append(list(map(int, input().split())))
+
+end_col = 0
+
+for i in range(n):
+    flag = True
+    for j in range(k-1, k-1+m):
+        if grid[i][j] == 1:
+            flag = False
+            break
+        end_col = i
+        break
+
+for j in range(k-1, k-1+m):
+    grid[end_col][j] = 1
+
+for i in range(4):
+    print(*grid[i])
