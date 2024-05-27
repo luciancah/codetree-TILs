4dff1c245eeb7bad9dc42e9b302124d2c62c2924
Jinhyung Lee <luciancah@gmail.com>
2024-05-27 14:26:05 +0900

    Title: 떨어지는 1자 블록, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240527/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240527/떨어지는 1자 블록/falling-horizontal-block.py
@@ -8,12 +8,12 @@ end_col = 0
 
 for i in range(n):
     flag = True
-    for j in range(0, k-1+m):
+    for j in range(k-1, k-1+m):
         if grid[i][j] == 1:
             flag = False
             break
         end_col = i
-        break
+    break
 
 for j in range(k-1, k-1+m):
     grid[end_col][j] = 1
