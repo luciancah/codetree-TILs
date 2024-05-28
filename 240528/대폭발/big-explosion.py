793d4a6d50bb5d08853aaa82c2c1ac101dab3be7
Jinhyung Lee <luciancah@gmail.com>
2024-05-28 13:17:05 +0900

    Title: 대폭발, Time: 175ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240528/대폭발/big-explosion.py
@@ -0,0 +1,43 @@
+from copy import deepcopy 
+
+n, m, r, c = list(map(int, input().split()))
+grid = [[0] * n for _ in range(n)]
+
+dxs, dys = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+def in_range(y, x):
+    return 0 <= y < n and 0 <= x < n
+
+def update_grid(grid, t):
+    new_grid = deepcopy(grid)
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j] == 1:
+                for k in range(4):
+                    ny, nx = i + dys[k]*t, j + dxs[k]*t
+                    # print(ny, nx, t, k)
+                    if in_range(ny, nx):
+                        new_grid[ny][nx] = 1
+
+    # for i in range(n):
+    #     print(*new_grid[i])
+    # print()
+    return new_grid
+
+y, x = r-1, c-1
+grid[y][x] = 1
+
+for t in range(m+1):
+    # print('t', t)
+    grid = update_grid(grid, t)
+
+
+count = 0
+
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1:
+            count += 1
+
+print(count)
