56a4d37c352626a8a6009388fb46865c5d3d51ba
Jinhyung Lee <luciancah@gmail.com>
2024-05-30 16:04:45 +0900

    Title: 강력한 폭발, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240530/강력한 폭발/strong-explosion.py
+++ 240530/강력한 폭발/strong-explosion.py
@@ -6,6 +6,9 @@ grid = []
 bombs = []
 bombs_placed = []
 bp2 = []
+bp3 = []
+
+bomb_spreads = [[[-1, 0], [-2, 0], [1, 0], [2, 0]], [[-1, 0], [1, 0], [0, 1], [0, -1]], [[-1, -1], [1, 1], [-1, 1], [1, -1]]]
 
 for _ in range(n):
     grid.append(list(map(int, input().split())))
@@ -13,37 +16,47 @@ for _ in range(n):
 def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
+def spread_bomb(r, c, t):
+    new_bombs = []
+    for i in range(4):
+        ny, nx = r + bomb_spreads[t][i][0], c + bomb_spreads[t][i][1]
+        if in_range(ny, nx):
+            new_bombs.append([ny, nx])
+
+    return new_bombs
+
 for r in range(n):
     for c in range(n):
         if grid[r][c] == 1:
             bombs.append([r, c])
 
-
-# bombs_count = 0
-
-# def place_bomb(bombs_count):
-#     if bombs_count == len(bombs):
-#         print(bombs_placed)
-#         return
-
-#     for i in range(1, 4):
-#         bombs_placed.append(i)
-#         place_bomb(bombs_count + 1)
-#         bombs_placed.pop
-    
-# place_bomb(0)
-
-
-def choose(curr_num):
-    if curr_num == len(bombs) + 1:
-        print(bombs_placed)
+def place_bomb(bombs_count):
+    if bombs_count == len(bombs) + 1:
+        bp3.append(bp2[:])
         return
 
-    for i in range(1, 4):
+    for i in range(3):
         bombs_placed.append(i)
-        choose(curr_num + 1)
+        bp2.append(spread_bomb(bombs[bombs_count-1][0], bombs[bombs_count-1][1], i))
+        place_bomb(bombs_count + 1)
+        bp2.pop()
         bombs_placed.pop()
+    
+place_bomb(1)
 
-    return
+for i in range(len(bp3)):
+    new_bp = []
+    for j in range(len(bp3[i])):
+        new_bp += *bp3[i][j]
+print(new_bp)
 
-choose(1)
+
+# [[[[0, 2], [2, 2], [3, 2]], [[1, 1], [0, 1], [3, 1]]]
+# [[[0, 2], [2, 2], [3, 2]], [[1, 1], [3, 1], [2, 2], [2, 0]]]
+# [[[0, 2], [2, 2], [3, 2]], [[1, 0], [3, 2], [1, 2], [3, 0]]]
+# [[[0, 2], [2, 2], [1, 3], [1, 1]], [[1, 1], [0, 1], [3, 1]]]
+# [[[0, 2], [2, 2], [1, 3], [1, 1]], [[1, 1], [3, 1], [2, 2], [2, 0]]]
+# [[[0, 2], [2, 2], [1, 3], [1, 1]], [[1, 0], [3, 2], [1, 2], [3, 0]]]
+# [[[0, 1], [2, 3], [0, 3], [2, 1]], [[1, 1], [0, 1], [3, 1]]]
+# [[[0, 1], [2, 3], [0, 3], [2, 1]], [[1, 1], [3, 1], [2, 2], [2, 0]]]
+# [[[0, 1], [2, 3], [0, 3], [2, 1]], [[1, 0], [3, 2], [1, 2], [3, 0]]]]
