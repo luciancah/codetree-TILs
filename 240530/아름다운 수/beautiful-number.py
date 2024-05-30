cc1c6024dd0de959feb87dc7fc6d8b46c865fe25
Jinhyung Lee <luciancah@gmail.com>
2024-05-30 14:52:23 +0900

    Title: 아름다운 수, Time: 343ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240530/아름다운 수/beautiful-number.py
@@ -0,0 +1,41 @@
+n = int(input())
+answer = []
+a2 = []
+
+def is_beautiful(num_arr):
+    flag = True
+    i = 0
+    while i < len(num_arr):
+        temp = num_arr[i]
+        count = 0
+        for j in range(i, i + temp):
+            if j >= len(num_arr):
+                flag = False
+                break
+            if num_arr[j] == temp:
+                count += 1
+        # print(i, j, flag)
+        if count != temp:
+            flag = False
+        i += temp
+    return flag
+
+# print(is_beautiful([2, 2, 3]))
+
+
+def choose(curr_num):
+    if curr_num == n + 1:
+        if is_beautiful(answer):
+            if is_beautiful(answer):
+                a2.append(answer[:])
+        return
+
+    for i in range(1, n+1):
+        answer.append(i)
+        choose(curr_num + 1)
+        answer.pop()
+
+    return
+
+choose(1)
+print(len(a2))
