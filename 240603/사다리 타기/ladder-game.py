a46fe2ee05bc11279ba45d937170d2203605f697
Jinhyung Lee <luciancah@gmail.com>
2024-06-03 14:48:16 +0900

    Title: 사다리 타기, Time: 200ms, Memory: 27MB, Status: Passed - CodeTree

--- 240603/사다리 타기/ladder-game.py
+++ 240603/사다리 타기/ladder-game.py
@@ -4,33 +4,35 @@ for i in range(m):
     lines.append(list(map(int, input().split())))
 
 def find_ladder_answer(lines):
-    grid = [[0] * m for _ in range(n)]
+    grid = [[0] * 15 for _ in range(n)]
 
-    for l in lines:
-        r, c = l
-        grid[r-1][c-1] = 1
-        grid[r][c-1] = -1
+    if len(lines) > 0:
+        for l in lines:
+            r, c = l
+            grid[r-1][c-1] = 1
+            grid[r][c-1] = -1
 
-    ans = []
+    re = []
 
     for k in range(n):
         dist = 0
-        for i in range(m):
+        for i in range(15):
             dist = dist + grid[k+dist][i]
-        ans.append(k+dist)
+        re.append(k+dist)
     
-    return ans
+    return re
 
 original_ans = find_ladder_answer(lines)
 
-ans = 6
 res = []
+ans = m
 
 def recur(count):
     global res, ans
 
     if count == m:
-        ans = min(ans, len(find_ladder_answer(res)))
+        if original_ans == find_ladder_answer(res):
+            ans = min(ans, len(res))
         return
     
     res.append(lines[count])
