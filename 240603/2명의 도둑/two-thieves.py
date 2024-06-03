ba3c208180a29dfc552690fe489f16bdf10ab79b
Jinhyung Lee <luciancah@gmail.com>
2024-06-03 16:53:52 +0900

    Title: 2명의 도둑, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240603/2명의 도둑/two-thieves.py
+++ 240603/2명의 도둑/two-thieves.py
@@ -1,21 +1,50 @@
-n, m, c = list(map(int, input().split()))
+from itertools import combinations
+
+n, m, z = list(map(int, input().split()))
 grid = []
 for _ in range(n):
     grid.append(list(map(int, input().split())))
 
 res = []
+res2 = []
+ans = 0
 
-def recur(count):
-    global res, ans
+def is_possible(res):
+    r1, r2 = res
+    if r1[0] == r2[0]:
+        if r2[1] - r1[1] < m:
+            return False
+    return True
 
-    if count == m:
-        print(res)
-        return
+pop_list = []
+for i in range(n):
+    for j in range(n-m+1):
+        pop_list.append([i,j])
+
+combs = list(combinations(pop_list, 2))
+combs2 = []
+for c in combs:
+    if is_possible(c):
+        combs2.append(c)
+
+r1_max = 0
+r2_max = 0
+for c in combs2:
+    r1, r2 = c
+    r1_list = list(grid[r1[0]][r1[1]+i] for i in range(m))
+    r2_list = list(grid[r2[0]][r2[1]+i] for i in range(m))
+
     
-    res.append(lines[count])
-    recur(count + 1)
-    res.pop()
+    for i in range(m, 0, -1):
+        r1_list2 = list(combinations(r1_list, i))
+        for r in r1_list2:
+            if sum(r) <= z:
+                r1_max = max(r1_max, sum([x*x for x in r]))
 
-    
+    for i in range(m, 0, -1):
+        r2_list2 = list(combinations(r2_list, i))
+        for r in r2_list2:
+            if sum(r) <= z:
+                r2_max = max(r2_max, sum([x*x for x in r]))
 
-recur(0)
+print(r1_max+r2_max)
