786b205a5f804917bc26ce55305d7a5c818477aa
Jinhyung Lee <luciancah@gmail.com>
2024-06-04 18:13:16 +0900

    Title: 1차원 윷놀이, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240604/1차원 윷놀이/yutnori-1d.py
+++ 240604/1차원 윷놀이/yutnori-1d.py
@@ -3,20 +3,28 @@ moves = list(map(int, input().split()))
 
 # moves를 n개의 말에 배분하는 조합
 
-answers = [[0] for _ in range(k)]
+answers = [1 for _ in range(k)]
 
+def calc(answers):
+    return len([x for x in answers if x >= m])
+
+max_ans = 0
 def choose(count):
+    global max_ans
     if count == 4:
-        print(answers)
+        max_ans = max(max_ans, calc(answers))
         return
 
-    for i in len(moves):
-        answers[j].append(moves[i])
-        t = moves.pop()
-        choose(count + 1)
-        answers[j].pop()
-        moves.append(t)
+    for i in range(len(answers)):
+        if answers[i] >= m:
+            continue
+        else:
+            answers[i] += moves[count]
+            choose(count + 1)
+            answers[i] -= moves[count]
 
     return
 
 choose(0)
+
+print(max_ans)
