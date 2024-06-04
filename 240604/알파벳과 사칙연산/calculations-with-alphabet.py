79ed6623bfb5645ecd82873c317cea81696c5d98
Jinhyung Lee <luciancah@gmail.com>
2024-06-04 13:37:14 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240604/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 240604/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -2,20 +2,19 @@ from collections import deque
 
 exp = input()
 expq = deque(exp)
+expq.appendleft('+')
+expq = list(expq)
 
 ans = []
 max_ans = 0
 
-expq.appendleft('+')
-expq = list(expq)
 
-def solve_exp(res, exp):
-    max_ans = -999
-    for j in range(1, 5):
-        ans = res
-
-        o1 = exp[0]
-        o2 = j
+def solve_exp(expq):
+    ans = 0
+    i = 0
+    while i < len(expq):
+        o1 = expq[i]
+        o2 = expq[i+1]
 
         if o1 == '+':
             ans += o2
@@ -23,12 +22,51 @@ def solve_exp(res, exp):
             ans -= o2
         else:
             ans *= o2
+        i+= 2
+    return ans
 
-        max_ans = max(max_ans, ans)
-    return max_ans
+# solve_exp(['+', '4', '-', '1', '*', '2'])
+nums = ['a', 'b', 'c', 'd']
 
-res = 0
-for i in range(0, len(expq)-1, 2):
-    res = solve_exp(res, expq[i:i+2])
+max_ans = 0
+for a in range(1, 5):
+    for b in range(1, 5):
+        for c in range(1, 5):
+            for d in range(1, 5):
+                # print(a, b, c, d)
+                l = expq[:]
+                for i in range(len(l)):
+                    # print(i, a, b, c, d)
+                    if l[i] == 'a':
+                        l[i] = a
+                    elif l[i] == 'b':
+                        # print('asf', l[i], b)
+                        l[i] = b
+                        # print('asf', l[i], b)
+                    elif l[i] == 'c':
+                        l[i] = c
+                    elif l[i] == 'd':
+                        l[i] = d
+                # print(l)
+                max_ans = max(max_ans, solve_exp(l))
 
-print(res)
+print(max_ans)
+
+# def recur(expq):
+#     global ans
+#     if len(expq) == 0:
+#         print(ans)
+#         return
+
+#     n = expq.popleft()
+#     if n in nums:
+#         for a in range(1, 5):
+#             ans.append(str(a))
+#             recur(expq)
+#             ans.pop()
+#     else:
+#         ans.append(n)
+#         recur(expq)
+#         ans.pop()
+
+# recur(expq)
