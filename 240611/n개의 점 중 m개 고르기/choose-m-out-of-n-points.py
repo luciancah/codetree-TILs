3c70fa7af058ef7a974300620c45fcdd61f05de0
Jinhyung Lee <luciancah@gmail.com>
2024-06-11 13:54:03 +0900

    Title: n개의 점 중 m개 고르기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240611/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240611/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,1 +1,38 @@
+import sys
+
 n, m = list(map(int, input().split()))
+dots = []
+for _ in range(n):
+    dots.append(list(map(int, input().split())))
+
+def calc_dist(a, b):
+    res = (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+    return res
+
+# 재귀 1사이클 : m개 될때까지 고를까 말까 / 안에서 가장 먼 두점 거리 구하기
+
+min_dist = sys.maxsize
+selected_dots = []
+
+def recur(count, curr_index, selected_dots):
+    global min_dist
+    if count == m:
+        for i in range(len(selected_dots)):
+            for j in range(i+1, len(selected_dots)):
+                dist = calc_dist(selected_dots[i], selected_dots[j])
+                min_dist = min(dist, min_dist)
+        # print(selected_dots)
+        return
+
+    if curr_index == n:
+        return
+
+    # 얘 고를지 말지
+    selected_dots.append(dots[curr_index])
+    recur(count+1, curr_index+1, selected_dots)
+    selected_dots.pop()
+    recur(count, curr_index+1, selected_dots)
+
+recur(0, 0, selected_dots)
+
+print(min_dist)
