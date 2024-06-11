c3d1e66cf33d721860902800795b96ba7972bfc7
Jinhyung Lee <luciancah@gmail.com>
2024-06-11 15:20:58 +0900

    Title: 외판원 순회, Time: 215ms, Memory: 26MB, Status: Passed - CodeTree

--- 240611/외판원 순회/traveling-salesman-problem.py
+++ 240611/외판원 순회/traveling-salesman-problem.py
@@ -6,30 +6,25 @@ for _ in range(n):
 visited = [False] * n
 visited[0] = True
 
-track = []
 min_dist = 999999999
 
-def recur(count, curr_pos, dist, track):
+def recur(count, curr_pos, dist):
     global min_dist
     if count == n:
         d = weight[curr_pos][0]
         if not d == 0:
-            # print(count, curr_pos, dist, d, track)
             min_dist = min(min_dist, d+dist)
             return
 
     for i in range(n):
         if not visited[i] and weight[curr_pos][i] != 0:
             d = weight[curr_pos][i]
-            track.append(i)
             visited[i] = True
             dist += d
-            # print('순회', i, d, dist, track, visited)
-            recur(count+1, i, dist, track)
+            recur(count+1, i, dist)
             visited[i] = False
             dist -= d
-            track.pop()
 
-recur(1, 0, 0, track)
+recur(1, 0, 0)
 
 print(min_dist)
