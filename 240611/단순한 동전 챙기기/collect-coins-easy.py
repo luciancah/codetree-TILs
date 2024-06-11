c21924b75f51d3505a8d3b04b8ce72600c3d27a7
Jinhyung Lee <luciancah@gmail.com>
2024-06-11 13:01:31 +0900

    Title: 단순한 동전 챙기기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240611/단순한 동전 챙기기/collect-coins-easy.py
+++ 240611/단순한 동전 챙기기/collect-coins-easy.py
@@ -1,3 +1,5 @@
+import sys
+
 n = int(input())
 grid = [input() for _ in range(4)]
 coins = {}
@@ -6,8 +8,31 @@ for i in range(n):
     for j in range(n):
         if grid[i][j].isnumeric():
             coins[int(grid[i][j])] = [i, j]
+        if grid[i][j] == 'S':
+            coins[0] = [i, j]
+        if grid[i][j] == 'E':
+            coins[99] = [i, j]
 
-coins = sorted(coins.items())
-print(coins)
+if len(coins) < 5:
+    print(-1)
+    sys.exit()
 
-# 현 위치에서 나보다 큰 애를 거쳐서 (최소 3개 거치도록 크게) E까지 가게 (1회 반복에 다음 코인 위치까지 이동
+def calc_dist(a, b):
+    res = abs(a[0] - b[0]) + abs(a[1] - b[1])
+    return res
+
+min_dist = 9999
+
+def recur(curr_coin, coin_count, move_dist):
+    global min_dist
+    if coin_count == 3:
+        move_dist += calc_dist(coins[curr_coin], coins[99])
+        min_dist = min(move_dist, min_dist)
+        return
+
+    for i in range(curr_coin+1, len(coins)-2):
+        recur(i, coin_count+1, move_dist+calc_dist(coins[curr_coin], coins[i]))
+
+recur(0, 0, 0)
+
+print(min_dist)
