bfba8280aa348e3982a3d16a3738552018c2ccdb
Jinhyung Lee <luciancah@gmail.com>
2024-06-13 16:50:23 +0900

    Title: 돌 잘 치우기, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240613/돌 잘 치우기/clear-stones-well.py
+++ 240613/돌 잘 치우기/clear-stones-well.py
@@ -2,7 +2,9 @@ from collections import deque
 from itertools import combinations
 
 n, k, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid2 = [list(map(int, input().split())) for _ in range(n)]
+empty = [[0] * n for _ in range(n)]
+# grid = grid2[:]
 starts = [list(map(int, input().split())) for _ in range(k)]
 starts = [[r-1, c-1] for r, c in starts]
 
@@ -16,18 +18,9 @@ def can_go(x, y):
         return False
     return True
 
-dxs, dys = [1, 0, 0, -1], [0, 1, -1, 0]
-
-def bfs():
-    while q:
-        x, y = q.popleft()
-
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-
-            if can_go(nx, ny):
-                q.append((x, y))
-                visited[x][y] = 1
+def push(x, y):
+    q.append((x, y))
+    visited[x][y] = 1
 
 def calc_visited(grid):
     res = 0
@@ -38,35 +31,53 @@ def calc_visited(grid):
 
     return res
 
+dxs, dys = [1, 0, 0, -1], [0, 1, -1, 0]
+
+def bfs():
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+
+            if can_go(nx, ny):
+                push(nx, ny)
 
 rocks = []
 for i in range(n):
     for j in range(n):
-        if grid[i][j] == 1:
+        if grid2[i][j] == 1:
             rocks.append([i, j])
 
-max_visited = 0
+# print(rocks)
 
-
-def create_grid(rocks):
-    new_grid = [[0] * n for _ in range(n)]
+def create_grid(r, grid):
     for i in range(n):
         for j in range(n):
-            if [i, j] in rocks:
-                new_grid[i][j] = 1
-    return new_grid
+            if [i, j] in r:
+                grid[i][j] = 1
+    return grid
 
 
 for nr in combinations(rocks, len(rocks)-m):
-    grid = create_grid(list(nr))
-    visited = [[0] * n for _ in range(n)]
+  # print(nr, rocks, len(rocks)-m)
+  empty = [[0] * n for _ in range(n)]
+  grid = create_grid(list(nr), empty)
 
-    for r, c in starts:
-        q = deque()
-        q.append((r, c))
-        visited[r][c] = 1
-        bfs()
+  # for i in range(n):
+  #     print(*grid[i])
+  # print()
 
-        max_visited = max(calc_visited(visited), max_visited)
+
+  visited = [[0] * n for _ in range(n)]
+  max_visited = 0
+
+  for r, c in starts:
+      q = deque()
+      push(r, c)
+      bfs()
+      # print('-------')
+      # print(calc_visited(visited))
+      max_visited = max(calc_visited(visited), max_visited)
 
 print(max_visited)
