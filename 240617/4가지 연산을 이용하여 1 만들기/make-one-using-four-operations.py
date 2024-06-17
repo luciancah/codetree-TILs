2eb4401378fff69d15b28c3546840468c025fd1f
Jinhyung Lee <luciancah@gmail.com>
2024-06-17 13:03:59 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 350ms, Memory: 63MB, Status: Passed - CodeTree

--- 240617/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240617/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,22 +1,51 @@
+from collections import deque
+
 n = int(input())
+min_ans = 99999
 
-def calc(n):
-    if n % 3 == 0:
-        return n // 3
-    if n % 2 == 0:
-        return n // 2
-    if (n+1) % 3 == 0:
-        return n + 1
-    return n - 1
+q = deque()
+visited = [0 for _ in range(2*n)]
+step = [0 for _ in range(2*n)]
 
-# def bfs():
-#     while n:
-#         depth = 0
+def in_range(num):
+    return 1 <= num <= 2*n-1
 
-#         for i in range(4):
+def can_go(num):
+    return in_range(num) and not visited[num]
 
-i = 0
-while n > 1:
-    n = calc(n)
-    i += 1
-print(i)
+# def calc(num, operator):
+#     if operator == 0:
+#         return num - 1
+#     if operator == 1:
+#         return num + 1
+#     if operator == 2:
+#         if num % 2 == 0:
+#             return num 
+
+def bfs():
+    while q:
+        curr_num = q.popleft()
+
+        for i in range(4):
+            if i == 0:
+                new_num = curr_num - 1
+            if i == 1:
+                new_num = curr_num + 1
+            if i == 2:
+                if curr_num % 2 == 0:
+                    new_num = curr_num // 2
+            if i == 3:
+                if curr_num % 3 == 0:
+                    new_num = curr_num // 3
+            
+            if can_go(new_num):
+                visited[new_num] = 1
+                step[new_num] = step[curr_num] + 1
+                q.append(new_num)
+
+    return step[1]
+
+visited[n] = 1
+step[n] = 1
+q.append(n)
+print(bfs()-1)
