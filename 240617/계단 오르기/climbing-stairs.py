55ad3246e43e073fc71108fae9e33fe33ef2c42a
Jinhyung Lee <luciancah@gmail.com>
2024-06-17 14:27:26 +0900

    Title: 계단 오르기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240617/계단 오르기/climbing-stairs.py
+++ 240617/계단 오르기/climbing-stairs.py
@@ -7,13 +7,15 @@ n = int(input())
 
 # 점화식: f(n) = f(n-2) + f(n-3)
 
-dp = [0] * 1001
+dp = [0] * (n+1)
+if n <5:
+    dp = [0] * 5
 dp[0] = 0
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
 
-for i in range(4, n):
+for i in range(4, n+1):
     dp[i] = dp[i-2] + dp[i-3]
 
 print(dp[n])
