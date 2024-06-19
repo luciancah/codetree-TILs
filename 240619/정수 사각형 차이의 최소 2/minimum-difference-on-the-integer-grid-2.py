1849b8d6e58f5876eb46271c5c1d94435d8a24dd
Jinhyung Lee <luciancah@gmail.com>
2024-06-19 15:04:51 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 5008ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

--- 240619/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240619/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -2,13 +2,16 @@ import sys
 
 INT_MAX = sys.maxsize
 
+# 변수 선언 및 입력:
 n = int(input())
 num = [list(map(int, input().split())) for _ in range(n)]
 dp = {}
 
 def initialize():
+    # 시작점의 경우 dp[(0, 0, num[0][0])] = num[0][0]으로 초기값을 설정해줍니다
     dp[(0, 0, num[0][0])] = num[0][0]
 
+    # 최좌측 열의 초기값을 설정해줍니다.
     for i in range(1, n):
         for k in list(dp.keys()):
             if k[0] == i - 1 and k[1] == 0:
@@ -19,6 +22,7 @@ def initialize():
                 else:
                     dp[(i, 0, new_min)] = min(dp[(i, 0, new_min)], new_max)
 
+    # 최상단 행의 초기값을 설정해줍니다.
     for j in range(1, n):
         for k in list(dp.keys()):
             if k[0] == 0 and k[1] == j - 1:
@@ -30,8 +34,11 @@ def initialize():
                     dp[(0, j, new_min)] = min(dp[(0, j, new_min)], new_max)
 
 def solve():
+    # DP 초기값 설정
     initialize()
 
+    # 탐색하는 위치의 위에 값과 좌측 값 중에 작은 값과
+    # 해당 위치의 숫자 중에 최댓값을 구해줍니다.
     for i in range(1, n):
         for j in range(1, n):
             new_states = {}
