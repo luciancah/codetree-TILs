3722f8ab99304d0e5eb89226f5e98a52b2107bd9
Jinhyung Lee <luciancah@gmail.com>
2024-06-19 14:37:48 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240619/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240619/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -22,22 +22,37 @@ init()
 
 for i in range(1, n):
     for j in range(1, n):
-        # dp[i][j]['mn'] = min(max(dp[i-1][j]['mn'], dp[i][j-1]['mn']), grid[i][j])
-        # dp[i][j]['mx'] = max(min(dp[i-1][j]['mx'], dp[i][j-1]['mx']), grid[i][j])
-
         p1 = abs(min(dp[i-1][j]['mn'], grid[i][j]) - max(dp[i-1][j]['mx'], grid[i][j]))
         p2 = abs(min(dp[i][j-1]['mn'], grid[i][j]) - max(dp[i][j-1]['mx'], grid[i][j]))
 
         if p1 < p2:
             dp[i][j]['mn'] = min(dp[i-1][j]['mn'], grid[i][j])
             dp[i][j]['mx'] = max(dp[i-1][j]['mx'], grid[i][j])
-        else:
+        elif p1 >= p2:
             dp[i][j]['mn'] = min(dp[i][j-1]['mn'], grid[i][j])
             dp[i][j]['mx'] = max(dp[i][j-1]['mx'], grid[i][j])
-
-
 
 # for i in range(n):
 #     print(*dp[i])
 
-print(abs(dp[-1][-1]['mx'] - dp[-1][-1]['mn']))
+
+a1 = abs(dp[-1][-1]['mx'] - dp[-1][-1]['mn'])
+
+dp = [[{'mx': 0, 'mn': 0} for _ in range(n)] for _ in range(n)]
+init()
+
+for i in range(1, n):
+    for j in range(1, n):
+        p1 = abs(min(dp[i-1][j]['mn'], grid[i][j]) - max(dp[i-1][j]['mx'], grid[i][j]))
+        p2 = abs(min(dp[i][j-1]['mn'], grid[i][j]) - max(dp[i][j-1]['mx'], grid[i][j]))
+
+        if p1 <= p2:
+            dp[i][j]['mn'] = min(dp[i-1][j]['mn'], grid[i][j])
+            dp[i][j]['mx'] = max(dp[i-1][j]['mx'], grid[i][j])
+        elif p1 > p2:
+            dp[i][j]['mn'] = min(dp[i][j-1]['mn'], grid[i][j])
+            dp[i][j]['mx'] = max(dp[i][j-1]['mx'], grid[i][j])
+
+b1 = abs(dp[-1][-1]['mx'] - dp[-1][-1]['mn'])
+
+print(min(a1, b1))
