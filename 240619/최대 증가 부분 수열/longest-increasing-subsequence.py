9dc8094f265df89d38c6e853d0b1937c7dfee630
Jinhyung Lee <luciancah@gmail.com>
2024-06-19 16:42:21 +0900

    Title: 최대 증가 부분 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240619/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(input())
+dp = [0] * n
+a = [list(map(int, input().split())) for _ in range(n)]
+
+def init():
+    for i in range(n):
+        dp[i] = -sys.maxsize
+
+    dp[0] = 0
+
+init()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if dp[j] == -sys.maxsize:
+            continue
+
+        if j + a[j] >= i:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+ans = 0
+for i in range(n):
+    ans = max(ans, dp[i])
+
+print(ans)
