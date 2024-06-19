971c2d0c89ea00713438b3740051362f4351b3d2
Jinhyung Lee <luciancah@gmail.com>
2024-06-19 17:21:44 +0900

    Title: 최대 점프 횟수, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240619/최대 점프 횟수/maximum-number-of-jumps.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(input())
+dp = [0] * n
+a = list(map(int, input().split()))
+
+def init():
+    for i in range(n):
+        dp[i] = -sys.maxsize
+    dp[0] = 1
+
+init()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if dp[j] == -sys.maxsize:
+            continue
+        if a[j] < a[i]:
+            dp[i] = max(dp[j] + 1, dp[i])
+
+print(max(dp))
