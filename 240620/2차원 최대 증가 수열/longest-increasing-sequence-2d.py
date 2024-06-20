c61084f6edd1e6b7ee532a753e8f259db54335b7
Jinhyung Lee <luciancah@gmail.com>
2024-06-20 14:05:31 +0900

    Title: 2차원 최대 증가 수열, Time: 133ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240620/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240620/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -4,23 +4,24 @@ dp = [[0 for _ in range(m)] for _ in range(n)]
 
 dp[0][0] = 1
 
-for i in range(n):
-    print(*dp[i])
-print()
+# for i in range(n):
+#     print(*dp[i])
+# print()
 
 for i in range(1, n):
     for j in range(1, n):
         new_count = 0
-        for k in range(0, i):
-            for l in range(0, j):
-                # if k == i or l == j:
-                #     continue
-                if grid[i][j] > grid[k][l]:
+        for k in range(0, i+1):
+            for l in range(0, j+1):
+                if k == i or l == j:
+                    continue
+                if grid[i][j] > grid[k][l] and dp[k][l] != 0:
+                    # print(i, j, k, l, grid[i][j], grid[k][l])
                     new_count = max(new_count, dp[k][l]+1)
         dp[i][j] = new_count
 
-for i in range(n):
-    print(*dp[i])
+# for i in range(n):
+#     print(*dp[i])
 
 ans = 0            
 for i in range(n):
