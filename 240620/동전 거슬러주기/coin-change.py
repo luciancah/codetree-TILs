f4aef97cf9b4b9f2a75687f695b9c562669b3ffa
Jinhyung Lee <luciancah@gmail.com>
2024-06-20 16:35:25 +0900

    Title: 동전 거슬러주기, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240620/동전 거슬러주기/coin-change.py
+++ 240620/동전 거슬러주기/coin-change.py
@@ -1,29 +1,26 @@
 import sys
 
-int_min = -sys.maxsize
-
 n, m = map(int, input().split())
 dp = [0] * (m+1)
-coin = list(map(int, input().split()))
+coin = [0] + list(map(int, input().split()))
 
 def init():
     for i in range(m+1):
-        dp[i] = int_min
-    
+        dp[i] = sys.maxsize
     dp[0] = 0
 
 init()
 
 for i in range(1, m+1):
-    for j in range(1, n):
+    for j in range(1, n+1):
         if i >= coin[j]:
-            if dp[i-coin[j]] == int_min:
+            if dp[i-coin[j]] == sys.maxsize:
                 continue
-            dp[i] = max(dp[i], dp[i-coin[j]] + 1)
+            dp[i] = min(dp[i], dp[i-coin[j]] + 1)
 
 ans = dp[m]
 
-if ans == int_min:
+if ans == sys.maxsize:
     ans = -1
 
 print(ans)
