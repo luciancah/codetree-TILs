f95a76a5754aa818818268556251146e8d96a249
Jinhyung Lee <luciancah@gmail.com>
2024-06-20 15:59:05 +0900

    Title: 알바로 부자 되기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240620/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -8,11 +8,11 @@ for _ in range(n):
 # dp 배열 : 해당 일자에 일 했을때 벌 수 있는 최대 돈
 
 dp[0] = grid[0][2]
-for i in range(n):
+for i in range(1, n):
     max_money = 0
     for j in range(0, i):
         if grid[j][1] < grid[i][0]:
             max_money = max(grid[i][2] + dp[j], max_money)
     dp[i] = max_money
-
+# print(dp)
 print(max(dp))
