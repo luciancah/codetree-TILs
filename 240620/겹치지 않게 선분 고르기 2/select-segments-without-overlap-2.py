04fe91c2ba0fdc695e498fe39f1b9a7577608539
Jinhyung Lee <luciancah@gmail.com>
2024-06-20 14:57:54 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240620/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -2,7 +2,7 @@ n = int(input())
 lines = []
 for _ in range(n):
     lines.append(list(map(int, input().split())))
-dp = [0] * n
+
 
 '''
 원래 접근 : combination 만들어서 걔네 겹치는지 확인 - 최댓값 구하기 (완탐)
@@ -21,13 +21,15 @@ def is_crossed(l1, l2):
     return False
 
 
-dp[0] = 1
-for i in range(n):
-    max_dp = 0
-    for j in range(0, i):
-        if not is_crossed(lines[i], lines[j]):
-            max_dp = max(max_dp, dp[j] + 1)
+for k in range(n):
+    dp = [0] * n
+    dp[k] = 0
+    for i in range(k, n):
+        max_dp = 0
+        for j in range(0, i):
+            if not is_crossed(lines[i], lines[j]):
+                max_dp = max(max_dp, dp[j] + 1)
 
-    dp[i] = max_dp
+        dp[i] = max_dp
 
 print(max(dp)+1)
