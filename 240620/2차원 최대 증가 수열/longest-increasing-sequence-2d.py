100d90c443d6eb47f96ca948e91ea534a22b6d60
Jinhyung Lee <luciancah@gmail.com>
2024-06-20 13:59:49 +0900

    Title: 2차원 최대 증가 수열, Time: 132ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240620/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240620/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -5,29 +5,38 @@ dp = [[0 for _ in range(m)] for _ in range(n)]
 dp[0][0] = 1
 
 # 초기화
-for i in range(n):
-    count = 0
-    for j in range(0, i+1):
-        count += 1
-        if grid[0][j] < grid[0][i]:
-            dp[0][j] = count
+# for i in range(1, n):
+#     count = 0
+#     for j in range(0, i+1):
+#         if grid[0][j] < grid[0][i]:
+#             count = max(dp[0][j] + 1, count)
+#         dp[0][i] = count
 
-for i in range(n):
-    for j in range(0, i+1):
-        count += 1
-        if grid[j][0] < grid[i][0]:
-            dp[j][0] = count
+
+# for i in range(1, n):
+#     count = 1
+#     for j in range(0, i+1):
+#         if grid[j][0] < grid[i][0]:
+#             count = max(dp[j][0] + 1, count)
+#         dp[i][0] = count
+
+# for i in range(n):
+#     print(*dp[i])
+# print()
 
 for i in range(1, n):
     for j in range(1, n):
         new_count = 0
         for k in range(0, i+1):
             for l in range(0, j+1):
-                if (k, l) == (i+1, j+1):
+                if k == i or l == j:
                     continue
                 if grid[i][j] > grid[k][l]:
                     new_count = max(new_count, dp[k][l]+1)
-                dp[i][j] = new_count
+        dp[i][j] = new_count
+
+# for i in range(n):
+#     print(*dp[i])
 
 ans = 0            
 for i in range(n):
