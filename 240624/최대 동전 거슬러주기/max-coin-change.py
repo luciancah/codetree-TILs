ba6efa881fcc6c40be62791606aac7e057f0d98b
Jinhyung Lee <luciancah@gmail.com>
2024-06-24 16:19:15 +0900

    Title: 최대 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240624/최대 동전 거슬러주기/max-coin-change.py
+++ 240624/최대 동전 거슬러주기/max-coin-change.py
@@ -6,7 +6,8 @@ dp = [0 for _ in range(m+1)]
 dp[0] = 0
 
 for i in range(n):
-    dp[coins[i]] = 1
+    if i <= coins[i]:
+        dp[coins[i]] = 1
 
 for i in range(1, m+1):
     for j in range(n):
