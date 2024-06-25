eb342210867ab0ed44ed2fe6e40e29a972010ea7
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 12:44:26 +0900

    Title: 배낭 채우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240625/배낭 채우기 2/knapsack-2.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+values = []
+weights = []
+n, m = map(int, input().split())
+for _ in range(n):
+    w, v = map(int, input().split())
+    values.append(v)
+    weights.append(w)
+
+# dp배열 : 무게 / 금액 / 개수 ?
+
+max_num = m // min(weights) + 1
+dp = [[-1 for _ in range(m+1)] for _ in range(max_num)]
+for i in range(max_num):
+    dp[i][0] = 0
+
+for i in range(max_num):
+    for j in range(1, m+1):
+        for k in range(i+1):
+            if j >= weights[k]:
+                if dp[i][j-weights[k]] == -1:
+                    continue
+                dp[i][j] = max(dp[i][j], dp[i][j-weights[k]] + values[k])
+
+print(max(dp[-1]))
+
+# for i in range(max_num):
+#     print(*dp[i])
