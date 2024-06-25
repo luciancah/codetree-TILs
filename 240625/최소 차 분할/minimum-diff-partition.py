9574c175728c285b19663c5c81f293c7608f9f39
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 13:51:05 +0900

    Title: 최소 차 분할, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240625/최소 차 분할/minimum-diff-partition.py
+++ 240625/최소 차 분할/minimum-diff-partition.py
@@ -3,15 +3,13 @@ nums = list(map(int, input().split()))
 
 # dp = a, b 그룹이라고 할 때 a 그룹에 n개 / 값은 두 그룹의 차
 
-dp = [1000000 for _ in range(n + 1)]
+dp = [0 for _ in range(n + 1)]
 dp[0] = 0
 sum_nums = sum(nums)
 
 for i in range(n):
     for j in range(n, -1, -1):
         if j >= i:
-            if dp[j-i] == 1000000:
-                continue
             sum_selected = dp[j-i] + nums[i]
             # print(i, j, sum_nums, sum_selected, sum_nums-dp[i])
             dp[j] = dp[i] if abs(abs(sum_nums - dp[j]) - dp[j]) <= abs(abs(sum_nums - sum_selected) - sum_selected) else sum_selected
