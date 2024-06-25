adca4b71903c7568e44c6dd8d0dfc73ac75d8ab0
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 16:27:41 +0900

    Title: 동일하게 분할, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/동일하게 분할/equal-partition.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = list(map(int, input().split()))
+sum_arr = sum(arr)
+
+dp = [0 for _ in range(sum_arr + 1)]
+dp[0] = 1
+
+for a in arr:
+    dp[a] = 1
+
+for i in range(sum_arr, -1, -1):
+    for j in range(n):
+        if i - j <= 0:
+            continue
+        if not dp[i-j]:
+            continue
+        dp[i] = 1
+
+ans = 'Yes' if dp[sum_arr // 2] else 'No'
+
+# print(dp)
+# print(ans)
