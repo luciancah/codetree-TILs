e285567410e48a5e8bbc82a251f6b0b1f7476ba9
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 16:10:39 +0900

    Title: 경험치를 빠르게 얻기, Time: 2019ms, Memory: 36MB, Status: Passed - CodeTree

--- 240625/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 240625/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -5,7 +5,7 @@ n, m = map(int, input().split())
 quests = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
 sum_value = 0
 for i in range(len(quests)):
-    sum_value += quests[i][1]
+    sum_value += quests[i][0]
 
 dp = [max_ans for _ in range(sum_value + 1)]
 dp[0] = 0
@@ -17,6 +17,8 @@ for i in range(1, n+1):
                 continue
             dp[j] = min(dp[j], dp[j-quests[i][0]] + quests[i][1])
 
+# print(m, sum_value)
+# print(dp[m:])
 if len(dp[m:]):
     print(min(dp[m:]))
 else:
