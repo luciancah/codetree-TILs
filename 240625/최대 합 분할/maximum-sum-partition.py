e83f579be6fd742984ca1a78116095cdaa1a83a8
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 15:17:47 +0900

    Title: 최대 합 분할, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240625/최대 합 분할/maximum-sum-partition.py
+++ 240625/최대 합 분할/maximum-sum-partition.py
@@ -14,28 +14,11 @@ for i in range(1, n+1):
             dp[i][j] = True
         if dp[i-1][j]:
             dp[i][j] = True
-            
 
+ans = -sys.maxsize
+for i in range(n):
+    for j in range(1, m):
+        if dp[i][j] and j == m-j:
+            ans = max(ans, j)
 
-
-# n = int(input())
-# arr = [0] + list(map(int, input().split()))
-
-# m = sum(arr)
-# dp = [[0] * (m + 1) for _ in range(n + 1)]
-# dp[0][0] = True
-
-# for i in range(1, n + 1):
-#     for j in range(m + 1):
-#         if j >= arr[i] and dp[i - 1][j - arr[i]]:
-#             dp[i][j] = True
-        
-#         if dp[i - 1][j]:
-#             dp[i][j] = True
-
-# ans = INT_MAX
-# for i in range(1, m):
-#     if dp[n][i]:
-#         ans = min(ans, abs(i - (m - i)))
-
-# print(ans)
+print(ans)
