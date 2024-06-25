f2bbc7ac1a2bc14b15269fcde75327db33915101
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 11:11:02 +0900

    Title: 행복한 수열의 개수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,33 @@
+n, m = map(int, input().split())
+mat = []
+for _ in range(n):
+    mat.append(input())
+res = 0
+
+for i in range(n):
+    prev = mat[i][0]
+    streak = 1
+    for k in range(1, n):
+        if prev == mat[i][k]:
+            streak += 1
+        else:
+            streak = 1
+        if streak >= m:
+            res += 1
+            break
+        prev = mat[i][k]
+
+for i in range(n):
+    prev = mat[0][i]
+    streak = 1
+    for k in range(1, n):
+        if prev == mat[k][i]:
+            streak += 1
+        else:
+            streak = 1
+        if streak >= m:
+            res += 1
+            break
+        prev = mat[k][i]
+
+print(res)
