2233d57b71ccbf2b69b8c43f3707810d91a66f0a
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 15:58:28 +0900

    Title: 경험치를 빠르게 얻기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+quests = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
+dp = [100000001 for _ in range(m + 1)]
+dp[0] = 0
+
+for i in range(1, n+1):
+    for j in range(m, -1, -1):
+        if j >= i:
+            if dp[j-i] == 100000001:
+                continue
+            dp[j] = min(dp[j], dp[j-quests[i][0]] + quests[i][1])
+
+print(dp)
