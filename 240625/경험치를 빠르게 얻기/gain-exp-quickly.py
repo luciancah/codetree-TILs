7700ee1b2769e69eefb92fbc3ed843b9b1d30a04
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 16:07:37 +0900

    Title: 경험치를 빠르게 얻기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240625/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 240625/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -1,14 +1,20 @@
+import sys
+max_ans = sys.maxsize
+
 n, m = map(int, input().split())
 quests = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
-dp = [100000001 for _ in range(m + 1)]
+sum_value = 0
+for i in range(len(quests)):
+    sum_value += quests[i][1]
+
+dp = [max_ans for _ in range(sum_value + 1)]
 dp[0] = 0
 
 for i in range(1, n+1):
-    for j in range(m, -1, -1):
-        if j >= i:
-            if dp[j-quests[i][0]] == 100000001:
+    for j in range(sum_value, -1, -1):
+        if j >= quests[i][0]:
+            if dp[j-quests[i][0]] == max_ans:
                 continue
             dp[j] = min(dp[j], dp[j-quests[i][0]] + quests[i][1])
 
-print(dp)
-print(dp[-1])
+print(min(dp[m:]))
