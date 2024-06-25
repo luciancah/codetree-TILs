61d302d964ae64e34aedcc9ef07735f307fa0006
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 15:43:02 +0900

    Title: 최대 합 분할, Time: 1800ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240625/최대 합 분할/maximum-sum-partition.py
+++ 240625/최대 합 분할/maximum-sum-partition.py
@@ -1,26 +1,31 @@
-import sys
+def max_equal_sum_partition_v2(n, nums):
+    # 총합 계산
+    total_sum = sum(nums)
+
+    # dp[i][j][k]: i개의 수를 사용해서 그룹 A의 합이 j, 그룹 B의 합이 k인 경우의 가능성
+    dp = [[[False] * (total_sum + 1) for _ in range(total_sum + 1)] for _ in range(n + 1)]
+    dp[0][0][0] = True  # 아무 수도 사용하지 않고 두 그룹의 합이 0이 되는 경우
+
+    for num in nums:
+        for i in range(n, 0, -1):
+            for j in range(total_sum - num + 1):
+                for k in range(total_sum - num + 1):
+                    if dp[i-1][j][k]:  # num을 C 그룹에 포함시키는 경우
+                        dp[i][j][k] = True
+                    if dp[i-1][j-num][k]:  # num을 A 그룹에 포함시키는 경우
+                        dp[i][j][k] = True
+                    if dp[i-1][j][k-num]:  # num을 B 그룹에 포함시키는 경우
+                        dp[i][j][k] = True
+
+    max_equal_sum = 0
+    for j in range(total_sum // 2 + 1):
+        for k in range(total_sum // 2 + 1):
+            if j == k and any(dp[i][j][k] for i in range(n + 1)):
+                max_equal_sum = max(max_equal_sum, j)
+
+    return max_equal_sum
 
 n = int(input())
-arr = [0] + list(map(int, input().split()))
-m = sum(arr)
+nums = list(map(int, input().split()))
 
-dp = [[0] * (m+1) for _ in range(n+1)]
-dp[0][0] = True
-
-for i in range(1, n+1):
-    for j in range(m+1):
-        if j >= arr[i] and dp[i-1][j-arr[i]]:
-            dp[i][j] = True
-        if dp[i-1][j]:
-            dp[i][j] = True
-
-ans = 0
-for i in range(n):
-    for j in range(1, m):
-        if dp[i][j] and (m - j * 2) in arr:
-            ans = max(ans, j)
-
-# for i in range(n):
-#     print(*dp[i])
-
-print(ans)
+print(max_equal_sum_partition_v2(n, nums))
