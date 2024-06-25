ed2b0744b5604e4f78d7cd5bd365cbdc7b99785b
Jinhyung Lee <luciancah@gmail.com>
2024-06-25 15:50:35 +0900

    Title: 최대 합 분할, Time: 446ms, Memory: 99MB, Status: Passed - CodeTree

--- 240625/최대 합 분할/maximum-sum-partition.py
+++ 240625/최대 합 분할/maximum-sum-partition.py
@@ -1,27 +1,32 @@
+import sys
+
+INT_MIN = -sys.maxsize
+OFFSET = 100000
+
 n = int(input())
-nums = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-total_sum = sum(nums)
+m = sum(arr)
+dp = [[0] * (m + 1 + OFFSET) for _ in range(n + 1)]
 
-# dp[i][j][k]: i개의 수를 사용해서 그룹 A의 합이 j, 그룹 B의 합이 k인 경우의 가능성
-dp = [[[False] * (total_sum + 1) for _ in range(total_sum + 1)] for _ in range(n + 1)]
-dp[0][0][0] = True  # 아무 수도 사용하지 않고 두 그룹의 합이 0이 되는 경우
+for i in range(n + 1):
+    for j in range(-m, m + 1):
+        dp[i][j + OFFSET] = INT_MIN
 
-for num in nums:
-    for i in range(n, 0, -1):
-        for j in range(total_sum - num + 1):
-            for k in range(total_sum - num + 1):
-                if dp[i-1][j][k]:  # num을 C 그룹에 포함시키는 경우
-                    dp[i][j][k] = True
-                if dp[i-1][j-num][k]:  # num을 A 그룹에 포함시키는 경우
-                    dp[i][j][k] = True
-                if dp[i-1][j][k-num]:  # num을 B 그룹에 포함시키는 경우
-                    dp[i][j][k] = True
+dp[0][0 + OFFSET] = 0
 
-max_equal_sum = 0
-for j in range(total_sum // 2 + 1):
-    for k in range(total_sum // 2 + 1):
-        if j == k and any(dp[i][j][k] for i in range(n + 1)):
-            max_equal_sum = max(max_equal_sum, j)
+def update(i, j, prev_i, prev_j, val):
+    if prev_j < -m or prev_j > m or dp[prev_i][prev_j + OFFSET] == INT_MIN:
+        return
+    
+    dp[i][j + OFFSET] = max(dp[i][j + OFFSET], dp[prev_i][prev_j + OFFSET] + val)
 
-print(max_equal_sum)
+for i in range(1, n + 1):
+    for j in range(-m, m + 1):
+        update(i, j, i - 1, j - arr[i], arr[i])
+
+        update(i, j, i - 1, j + arr[i], 0)
+
+        update(i, j, i  - 1, j, 0)
+
+print(dp[n][0 + OFFSET])
