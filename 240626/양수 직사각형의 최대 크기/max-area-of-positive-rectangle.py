1813e795a31fd1cb8e8763a1e5582ea30d764cf5
Jinhyung Lee <luciancah@gmail.com>
2024-06-26 14:54:05 +0900

    Title: 양수 직사각형의 최대 크기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240626/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -22,4 +22,5 @@ for x0 in range(n):
                 if is_positive(x0, y0, x1, y1):
                     max_ans = max((x1-x0+1)*(y1-y0+1), max_ans)
     
+max_ans = max_ans if max_ans >= 0 else -1
 print(max_ans)
