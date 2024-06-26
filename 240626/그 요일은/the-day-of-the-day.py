0c2178cc03ff89268e8765e3d1ec0d03e635d000
Jinhyung Lee <luciancah@gmail.com>
2024-06-26 18:13:13 +0900

    Title: 그 요일은, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240626/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,26 @@
+m1, d1, m2, d2 = map(int, input().split())
+day = input()
+day_map = {
+    'Mon': 1,
+    'Tue': 2,
+    'Wed': 3,
+    'Thu': 4,
+    'Fri': 5,
+    'Sat': 6,
+    'Sun': 7
+}
+months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+ans = 0
+for i in range(m1, m2+1):
+    if i == m1:
+        ans += months[i] - d1
+        continue
+    if i == m2:
+        ans += d2
+        continue
+    ans += months[i]
+
+# print(ans)
+
+print((ans + day_map[day]-1) // 7)
