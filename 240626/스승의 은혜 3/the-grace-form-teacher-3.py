1eca936beef404cc0fd30cab304a40e4989f8055
Jinhyung Lee <luciancah@gmail.com>
2024-06-26 17:37:53 +0900

    Title: 스승의 은혜 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240626/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240626/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,23 +1,23 @@
 n, m = map(int, input().split())
 gifts = [list(map(int, input().split())) for _ in range(n)]
 gifts2 = gifts[:]
-gifts.sort(key=lambda x: (x[0]+x[1]))
+# gifts.sort(key=lambda x: (x[0]+x[1]))
 gifts2.sort(key=lambda x: (x[0] // 2 + x[1]))
 
-max_ans = 0
-for i in range(n):
-    price = 0
-    ans = n
-    for j in range(n):
-        if i == j:
-            price += gifts[j][0] // 2 + gifts[j][1]
-        else:
-            price += gifts[j][0] + gifts[j][1]
-        if price > m:
-            ans = j
-            # print(i, j, price, m, ans, max_ans)
-            break
-    max_ans = max(ans, max_ans)
+# max_ans = 0
+# for i in range(n):
+#     price = 0
+#     ans = n
+#     for j in range(n):
+#         if i == j:
+#             price += gifts[j][0] // 2 + gifts[j][1]
+#         else:
+#             price += gifts[j][0] + gifts[j][1]
+#         if price > m:
+#             ans = j
+#             # print(i, j, price, m, ans, max_ans)
+#             break
+#     max_ans = max(ans, max_ans)
 
 max_ans2 = 0
 for i in range(n):
