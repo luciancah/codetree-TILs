c0776279563bc7ebacff52ffb770014ba67231fd
Jinhyung Lee <luciancah@gmail.com>
2024-06-26 15:59:57 +0900

    Title: 양수 직사각형의 최대 크기, Time: 218ms, Memory: 26MB, Status: Passed - CodeTree

--- 240626/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240626/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,4 +1,3 @@
-from itertools import combinations
 n, m = map(int,input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 positive_dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
@@ -11,6 +10,8 @@ for i in range(1,n+1):
             positive_dp[i][j] = positive_dp[i-1][j] + positive_dp[i][j-1] - positive_dp[i-1][j-1]
 
 
+
+
 def is_positive_rectangle(sx,sy,ex,ey,size):
     positive = positive_dp[ex][ey]-positive_dp[ex][sy-1]-positive_dp[sx-1][ey] + positive_dp[sx-1][sy-1]
     if size == positive:
@@ -18,15 +19,16 @@ def is_positive_rectangle(sx,sy,ex,ey,size):
     return False
 
 ans = -1
-for ax in range(1,n):
-    for ay in range(1,m):
-        for bx in range(ax,n+1):
-            for by in range(ay,m+1):
-                row = abs(ax-bx) + 1
-                col = abs(ay-by) + 1
-                size = row * col
-                if is_positive_rectangle(ax,ay,bx,by,size):
-                    ans = max(ans, size)
+for ax in range(1,n+1):
+    for ay in range(1,m+1):
+        for bx in range(1,n+1):
+            for by in range(1,m+1):
+                if ax <= bx or ay<=by:
+                    row = abs(ax-bx) + 1
+                    col = abs(ay-by) + 1
+                    size = row * col
+                    if is_positive_rectangle(ax,ay,bx,by,size):
+                        ans = max(ans, size)
 print(ans)
 
 
