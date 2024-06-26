e74211bf49f8ce89b6508165be9c0e227db81285
Jinhyung Lee <luciancah@gmail.com>
2024-06-26 22:35:50 +0900

    Title: 기울어진 직사각형, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240626/기울어진 직사각형/slanted-rectangle.py
+++ 240626/기울어진 직사각형/slanted-rectangle.py
@@ -7,28 +7,33 @@ max_sum = 0
 
 def get_max_sum_in_range(row, col, k):
     if row < k - 1 or col < k - 1: return 0
-    range_max = 0
-    for c in range(1, k-1):
-        # print(k)
-        # print(row, col, row-k+1, col-k+1, c)
-        # print(row-k+1, col, row, col-k+1, c)
-        range_max = max(range_max, sum([
-            mat[i][j]
-            for i in range(n)
-            for j in range(n)
-            if i <= row and j <= col
-            and i >= row-k+1 and j >= col-k+1
-             and (abs(row-i)+abs(col-j) == c or abs(row-k+1 - i)+abs(col-k+1 - j) == c)
-        ]))
-        range_max = max(range_max, sum([
-            mat[i][j]
-            for i in range(n)
-            for j in range(n)
-            if i <= row and j <= col
-            and i >= row-k+1 and j >= col-k+1
-             and (abs(row-k+1 - i)+abs(col-j) == c or abs(row-i)+abs(col-k+1 - j) == c)
-        ]))
-    return range_max
+    dxs, dys = [1, -1, -1, 1], [-1, -1, 1, 1]
+    dx, dy = dxs[0], dys[0]
+    x = col - k + 2
+    y = row
+    # print(y, x, k)
+    k_range_max = 0
+    for x in range(col - k + 2, col):
+        range_max = 0
+        i = 0
+        x_offset = x
+        y_offset = y
+        while True:
+            if col-k+1 <= x_offset <= col and row-k+1 <= y_offset <= row:
+                # if k == 5:
+                #     print(y_offset, x_offset)
+                range_max += mat[y_offset][x_offset]
+            else:
+                x_offset -= dx
+                y_offset -= dy
+                i += 1
+                dx, dy = dxs[i], dys[i]
+            x_offset += dx
+            y_offset += dy
+            if x_offset == x and y_offset == y: break
+        # print(range_max)
+        k_range_max = max(range_max, k_range_max)
+    return k_range_max
 
 for row in range(n):
     for col in range(n):
