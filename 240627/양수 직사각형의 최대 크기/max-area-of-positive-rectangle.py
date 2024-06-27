919881084331686e60d2dc7d7ec7fd7cfffe468e
Jinhyung Lee <luciancah@gmail.com>
2024-06-27 15:03:22 +0900

    Title: 양수 직사각형의 최대 크기, Time: 187ms, Memory: 26MB, Status: Passed - CodeTree

--- 240627/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240627/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -3,14 +3,16 @@ def search_section(x, y, dest_x, dest_y):
     for i in range(y, dest_y + 1):
         for j in range(x, dest_x + 1):
             if mat[i][j] <= 0:
+                # print('no')
                 return 0
-    return (dest_x - x + 1) * (dest_y - y + 1)
+    # print((dest_x - x + 1) * (dest_y - y + 1))
+    return abs(dest_x - x + 1) * abs(dest_y - y + 1)
 
 def get_max_rec(row, col):
     range_max = 0
     x_offset, y_offset = col, row
-    for y_offset in range(n):
-        for x_offset in range(m):
+    for y_offset in range(row, n):
+        for x_offset in range(col, m):
             range_max = max(range_max, search_section(col, row, x_offset, y_offset))
     return range_max
 
