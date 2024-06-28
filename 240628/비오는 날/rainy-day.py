2349db6436228e3286e4f9a803e5c5492f33fc6e
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 13:26:16 +0900

    Title: 비오는 날, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/비오는 날/rainy-day.py
+++ 240628/비오는 날/rainy-day.py
@@ -4,5 +4,5 @@ days = [list(map(str, input().split())) for _ in range(n)]
 days.sort(key=lambda x:x[0])
 for i in range(n):
     if days[i][2] == 'Rain':
-        print(days[i])
+        print(' '.join(days[i]))
         break
