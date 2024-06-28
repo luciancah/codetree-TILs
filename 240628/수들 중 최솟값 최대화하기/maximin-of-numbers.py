8a3e23f21b639fbbd922376f055a9eccb8ad53f0
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 16:55:43 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240628/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,34 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+
+def recur(cnt, xidx, yidx, selected):
+    global ans
+    if cnt == n**2:
+        return
+    # if xidx == n and yidx == n:
+    #     return
+    if len(selected) == n:
+        # print(ans)
+        ans = max(ans, min(selected))
+        return
+
+    selected.append(grid[xidx][yidx])
+    recur(cnt+1, xidx+1, yidx+1, selected)
+    selected.pop()
+
+    selected.append(grid[xidx][yidx])
+    recur(cnt+1, xidx+1, yidx, selected)
+    selected.pop()
+
+    selected.append(grid[xidx][yidx])
+    recur(cnt+1, xidx, yidx+1, selected)
+    selected.pop()
+
+    selected.append(grid[xidx][yidx])
+    recur(cnt+1, xidx, yidx, selected)
+    selected.pop()
+
+recur(0, 0, 0, [])
+print(ans)
