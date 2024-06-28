64d499a6a30ee880258f9e4d8c6d015be16db9a8
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 17:25:24 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 252ms, Memory: 26MB, Status: Passed - CodeTree

--- 240628/수들 중 최솟값 최대화하기/maximin-of-numbers.py
+++ 240628/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -1,45 +1,30 @@
+import sys
+
+sys.setrecursionlimit(600000)
+
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-grid2 = list(map(list, zip(*grid[::-1])))
-
-# print(grid)
-# print(grid2)
+visited = [0 for _ in range(n)]
 
 ans = 0
+selected = []
 
-def recur(cnt, xidx, yidx, selected, grid):
-    # print(cnt, xidx, yidx)
+def recur(row):
     global ans
-    if cnt == n**2:
-        return
 
-    if xidx >= n or yidx >= n:
-        return
-    
     if len(selected) == n:
         ans = max(ans, min(selected))
         return
-
-    selected.append(grid[xidx][yidx])
-    recur(cnt+1, xidx+1, yidx+1, selected, grid)
-    selected.pop()
-    recur(cnt+1, xidx, yidx, selected, grid)
-
-    selected.append(grid[xidx][yidx])
-    recur(cnt+1, xidx+1, yidx, selected, grid)
-    selected.pop()
-    recur(cnt+1, xidx, yidx, selected, grid)
-
-    selected.append(grid[xidx][yidx])
-    recur(cnt+1, xidx, yidx+1, selected, grid)
-    selected.pop()
-    recur(cnt+1, xidx, yidx, selected, grid)
+    
+    for i in range(n):
+        if not visited[i]:
+            selected.append(grid[row][i])
+            visited[i] = 1
+            recur(row + 1)
+            selected.pop()
+            visited[i] = 0
 
     return
 
-recur(0, 0, 0, [], grid)
-a = ans
-recur(0, 0, 0, [], grid2)
-b = ans
-
-print(max(a, b))
+recur(0)
+print(ans)
