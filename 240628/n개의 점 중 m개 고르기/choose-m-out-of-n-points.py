acb655e824eefdda5ac608b493684602fce1e30c
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 16:43:44 +0900

    Title: n개의 점 중 m개 고르기, Time: 199ms, Memory: 27MB, Status: Passed - CodeTree

--- 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -2,33 +2,33 @@ import sys
 
 sys.setrecursionlimit(10000)
 
-n, _ = map(int, input().split())
+n, m = map(int, input().split())
 dots = [list(map(int, input().split())) for _ in range(n)]
-m = 2
 
 def calc_dist(d1, d2):
     return (d1[0]-d2[0]) ** 2 + (d1[1]-d2[1]) ** 2
 
 ans = sys.maxsize
-selected = []
 
-def recur(count):
+def recur(count, idx, selected):
     global ans
     dist = 0
     if len(selected) == m:
-        ans = min(ans, calc_dist(selected[0], selected[1]))
+        # print(selected)
+        for i in range(m):
+            for j in range(i+1, m):
+                dist = max(dist, calc_dist(selected[i], selected[j]))
+        ans = min(ans, dist)
         return
-    if count == m:
+    if idx == n:
         return
     
-    for i in range(n):
-        if dots[i] not in selected:
-            selected.append(dots[i])
-            recur(count + 1)
-            selected.pop()
-            recur(count + 1)
+    selected.append(dots[idx])
+    recur(count+1, idx+1, selected)
+    selected.pop()
+    recur(count, idx+1, selected)
 
     return
 
-recur(0)
+recur(0, 0, [])
 print(ans)
