6a8fca69e3421d2048ba816963db6261f706b1f6
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 13:25:43 +0900

    Title: 비오는 날, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240628/비오는 날/rainy-day.py
@@ -0,0 +1,8 @@
+n = int(input())
+days = [list(map(str, input().split())) for _ in range(n)]
+
+days.sort(key=lambda x:x[0])
+for i in range(n):
+    if days[i][2] == 'Rain':
+        print(days[i])
+        break
