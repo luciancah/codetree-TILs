3f2f2a670095ff0db9a52ba1793d43165ae9a860
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 15:40:59 +0900

    Title: n개의 점 중 m개 고르기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -1,5 +1,7 @@
 import sys
 
+sys.setrecursionlimit(10000)
+
 n, m = map(int, input().split())
 dots = [list(map(int, input().split())) for _ in range(n)]
 
@@ -11,8 +13,15 @@ selected = []
 
 def recur(count):
     global ans
+    dist = 0
+    if len(selected) == m:
+        # print(selected)
+        for i in range(m):
+            for j in range(i+1, m):
+                dist = max(dist, calc_dist(dots[i], dots[j]))
+                ans = min(ans, dist)
+        return
     if count == m:
-        print(selected)
         return
     
     for i in range(n):
@@ -22,3 +31,4 @@ def recur(count):
         recur(count + 1)
 
 recur(0)
+print(ans)
