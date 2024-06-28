634c33b8377e79a6a42545b8ccee48e269eaa046
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 11:07:07 +0900

    Title: 최솟값 3개, Time: 482ms, Memory: 31MB, Status: Passed - CodeTree

--- 240628/최솟값 3개/top-3-smallest-number.py
+++ 240628/최솟값 3개/top-3-smallest-number.py
@@ -4,13 +4,28 @@ n = int(input())
 nums = list(map(int, input().split()))
 
 heap = []
+
+def mul_smallest_three(nums):
+    res = 1
+    temp = []
+    for _ in range(3):
+        temp.append(heappop(heap))
+        # print(temp[-1])
+        res *= temp[-1]
+    
+    for i in range(3):
+        heappush(heap, temp[i])
+
+    return res
+
+
+
+
+
 for i in range(len(nums)):
     heappush(heap, nums[i])
     if i >=2:
-        res = 1
-        for j in range(3):
-            res *= heap[j]
-        print(res)
+        print(mul_smallest_three(heap))
     else:
         print(-1)
 
