0e9b684040ecce202629211f90d8cc7d7dc8bb0c
Jinhyung Lee <luciancah@gmail.com>
2024-06-28 16:25:25 +0900

    Title: n개의 점 중 m개 고르기, Time: 5032ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 240628/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -3,23 +3,24 @@ import sys
 sys.setrecursionlimit(10000)
 
 n, m = map(int, input().split())
-dots = [list(map(int, input().split())) for _ in range(n)]
+dots = [tuple(map(int, input().split())) for _ in range(n)]
 
 def calc_dist(d1, d2):
     return (d1[0]-d2[0]) ** 2 + (d1[1]-d2[1]) ** 2
 
 ans = sys.maxsize
-selected = []
+selected = set()
 
 def recur(count):
     global ans
     dist = 0
     if len(selected) == m:
         # print(selected)
-        for i in range(m):
-            for j in range(i+1, m):
-                # print(dots[i], dots[j], calc_dist(selected[i], selected[j]))
-                dist = max(dist, calc_dist(selected[i], selected[j]))
+        for i in selected:
+            for j in selected:
+                if i==j:
+                    continue
+                dist = max(dist, calc_dist(i, j))
         ans = min(ans, dist)
         return
     if count == m:
@@ -27,9 +28,9 @@ def recur(count):
     
     for i in range(n):
         if dots[i] not in selected:
-            selected.append(dots[i])
+            selected.add(dots[i])
             recur(count + 1)
-            selected.pop()
+            selected.remove(dots[i])
             recur(count + 1)
 
     return
