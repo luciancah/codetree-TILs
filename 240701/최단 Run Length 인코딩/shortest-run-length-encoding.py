ee899c0be63c985357a31637cea491335d94817e
Jinhyung Lee <luciancah@gmail.com>
2024-07-01 11:49:03 +0900

    Title: 최단 Run Length 인코딩, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240701/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,30 @@
+A = input()
+# If life gives you lemon...
+def get_run_length(buf):
+    res = ''
+    i = 0
+    while i < len(buf):
+        c = buf[i]
+        k = i
+        while i < len(buf) - 1 and buf[i] == buf[i + 1]:
+            i += 1
+        res += c + str(i-k+1)
+        i += 1
+    # print(res)
+    return res
+
+def shift(buf):
+    res = buf[0:-1]
+    res = buf[-1] + res
+    return res
+
+min_len = 10
+
+for _ in range(len(A)):
+    curr = get_run_length(A)
+    if len(curr) < min_len:
+        min_len = len(curr)
+        # print(curr)
+    A = shift(A)
+
+print(min_len)
