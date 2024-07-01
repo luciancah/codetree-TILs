524263b0d906739b9815d24df0e082da5d4bcc2e
Jinhyung Lee <luciancah@gmail.com>
2024-07-01 16:47:14 +0900

    Title: 둘 중 하나 잘 고르기, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 240701/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -1,19 +1,21 @@
 n = int(input())
 arr =[[0,0]] +  [list(map(int, input().split())) for _ in range(n*2)]
 
-# dp[i][j] = 빨간색 i개 파란색 j개 뽑았을때 최대의 합, 답: dp[n][n]
+# dp[i][j] = 빨간색 i번까지 봤을 때 빨간색 j개 뽑았을때 최대의 합, 답: dp[2n][n]
 
-dp = [[-1] * (n*2+1) for _ in range(n*2+1)]
+dp = [[-1] * (2*n+1) for _ in range(2*n+1)]
 dp[0][0] = 0
 
-for i in range(1, n*2+1):
-    dp[i][0] = dp[i-1][0] + arr[i][0]
-    dp[0][i] = dp[0][i-1] + arr[i][1]
+# for i in range(1, 2*n+1):
+#     dp[i][0] = dp[i-1][0] + arr[i][0]
+#     dp[0][i] = dp[0][i-1] + arr[i][1]
 
-for i in range(1, n*2+1):
-    for j in range(1, n*2+1):
-        dp[i][j] = max(dp[i-1][j] + arr[i][0], dp[i][j-1] + arr[i][1])
+for i in range(1, 2*n+1):
+    for j in range(i+1):
+        # # i번 빨간색 뽑고 j 안뽑는 경우
+        if j > 0:
+            dp[i][j] = max(dp[i-1][j-1] + arr[i][0], dp[i][j])
+        if i -j > 0:
+            dp[i][j] = max(dp[i-1][j] + arr[i][1], dp[i][j])
 
-print(dp)
-
-# [[0, 4, 6, 10, 13], [5, 9, 13, 17, 21], [10, 14, 18, 22, 26], [16, 20, 24, 28, 32], [19, 23, 27, 31, 35]]
+print(dp[2*n][n])
