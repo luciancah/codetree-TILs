e8a2435babd8f946c0826023aab6e69b55714e31
Jinhyung Lee <luciancah@gmail.com>
2024-07-01 14:15:17 +0900

    Title: 계단 오르기 2, Time: 144ms, Memory: 25MB, Status: Passed - CodeTree

--- 240701/계단 오르기 2/climbing-stairs-2.py
+++ 240701/계단 오르기 2/climbing-stairs-2.py
@@ -16,18 +16,21 @@ n = int(input())
 arr = [0] + list(map(int, input().split()))
 
 dp = [[int_min]  * (4) for _ in range(n+1)]
-for i in range(n+1):
-    dp[i][0] = 0
-dp[1][0] = 0
+
+dp[0][0] = 0
+dp[1][0] = int_min
 dp[1][1] = arr[1]
 dp[2][0] = arr[2]
 
 for i in range(2, n+1):
-    for j in range(1, 4):
-        if i == 4 and j == 0:
-            print(dp[i-1][j-1] + arr[i], dp[i-2][j] + arr[i])
-        dp[i][j] = max(dp[i-1][j-1] + arr[i], dp[i-2][j] + arr[i])
-    
+    for j in range(4):
+        # if i == 4 and j == 0:
+        #     print(dp[i-1][j-1] + arr[i], dp[i-2][j] + arr[i], dp[i-1][j-1], arr[i])
+        if j-1 >= 0 and i-1 >= 0 and dp[i-1][j-1] != int_min:
+            dp[i][j] = dp[i-1][j-1] + arr[i]
+        if i-2 >= 0 and j >=0 and dp[i-2][j] != int_min:
+            dp[i][j] = max(dp[i][j], dp[i-2][j] + arr[i])
+
 # print(dp)
 print(max(dp[-1]))
 # 0 [0, x, x, x]
@@ -35,3 +38,11 @@ print(max(dp[-1]))
 # 2 [2, 2, 3, x]
 # 3 [3, 5, 5, 6]
 # 4 [10, 7, 9, 9]
+
+# 0 [0, x, x, x]
+# 3 [x, 3, x, x]
+# 7 [7, x, 10, x]
+# 11 [x, 18, x, 21]
+# 2 [9, x, 20, x]
+# 9 [x, 27, x, 30]
+# 6 [15, x, 33, x]
